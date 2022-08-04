05bd2599dbf5376d717883b0605024e6cd05d736
esp-binhng98 <binh.nguyen@eastplayers.io>
2022-08-05 01:02:46 +0700

    call-Coingeko-API

--- src/entries/contentScript/main.ts
+++ src/entries/contentScript/main.ts
@@ -3,7 +3,8 @@
 // import browser from "webextension-polyfill";
 import Mark from "mark.js";
 import "@webcomponents/webcomponentsjs/webcomponents-bundle.js";
-// import { coinGeko } from "./views/network";
+import { coinGeko } from "./views/network";
+import { get } from "lodash"
 
 import "./views/AddressHighlight.svelte";
 import "./views/TrxHighlight.svelte";
@@ -62,24 +63,42 @@ const listPage: { [key: string]: string }[] = [
   },
 ];
 
+const getCoinList = async () => {
+  let response;
+  try {
+    response = await coinGeko.get("/coins/list");
+  } catch (e) {
+    console.error(e);
+  }
+  const data = get(response, "data") || [];
+  return [
+    ...data.map((item: any) => item.symbol.toUpperCase()),
+    ...data.map((item: any) => item.name),
+  ];
+};
+
 // Firefox `browser.tabs.executeScript()` requires scripts return a primitive value
 (async () => {
   // const coinList: { [key: string]: string | number }[] =
   //   (await sendMessage("coinList", { limit: 500 })) || [];
+
+  const coinList = (await getCoinList()) || [];
 
   // const nameAndSymbolList: string[] = [
   //   ...coinList.map((item: any) => item.symbol.toUpperCase()),
   //   ...coinList.map((item: any) => item.name),
   // ];
 
-  // const regexNativeToken = new RegExp(
-  //   nameAndSymbolList
-  //     .map(function (w) {
-  //       return escapeRegex(w);
-  //     })
-  //     .join("|"),
-  //   "g"
-  // );
+  // console.log("nameAndSymbolList: ", nameAndSymbolList);
+
+  const regexNativeToken = new RegExp(
+    coinList
+      .map(function (w) {
+        return escapeRegex(w);
+      })
+      .join("|"),
+    "g"
+  );
 
   console.info("[Nimbus 🌩] Hello world from content script");
 
@@ -141,41 +160,43 @@ const listPage: { [key: string]: string }[] = [
       });
     })();
 
-    // (() => {
-    //   const selectedPageSupport = listPage.find(
-    //     (item: { [key: string]: string }) => currentUrl.includes(item.url)
-    //   );
+    (() => {
+      const selectedPageSupport = listPage.find(
+        (item: { [key: string]: string }) => currentUrl.includes(item.url)
+      );
 
-    //   const context = document.querySelector(
-    //     `${selectedPageSupport?.selector}`
-    //   );
-    //   const instance = new Mark(context);
-    //   // /0x[a-fA-F0-9]{64}/g
-    //   instance.markRegExp(regexNativeToken, {
-    //     // instance.markRegExp(/BTC|ETH|Bitcoin/g, {
-    //     element: "native-token-highlight",
-    //     className: "nimbus-ext",
-    //     exclude: ["[data-markjs]", ".nimbus-ext", "native-token-info"],
-    //     // acrossElements: true,
-    //     debug: true,
-    //     each(item: any) {
-    //       const selectedItem = coinList.find(
-    //         (data: { [key: string]: string | number }) =>
-    //           data.symbol === item.innerText || data.name === item.innerText
-    //       );
+      const context = document.querySelector(
+        `${selectedPageSupport?.selector}`
+      );
+      const instance = new Mark(context);
+      // /0x[a-fA-F0-9]{64}/g
+      instance.markRegExp(regexNativeToken, {
+        // instance.markRegExp(/BTC|ETH|Bitcoin/g, {
+        element: "native-token-highlight",
+        className: "nimbus-ext",
+        exclude: ["[data-markjs]", ".nimbus-ext", "native-token-info"],
+        // acrossElements: true,
+        debug: true,
+        each(item: any) {
+          const selectedItem = coinList.find(
+            (data: { [key: string]: string | number }) =>
+              data.symbol === item.innerText || data.name === item.innerText
+          );
 
-    //       // Inject address as props
-    //       item.setAttribute("id", selectedItem?.id);
-    //       item.setAttribute("name", item.innerText);
-    //     },
-    //     filter: function (textNode: any, foundTerm: string, totalCounter: any) {
-    //       return true; // must return either true or false
-    //     },
-    //     done() {
-    //       // console.log("Done mark addresses");
-    //     },
-    //   });
-    // })();
+          console.log("selectedItem: ", selectedItem)
+
+          // Inject address as props
+          item.setAttribute("id", selectedItem?.id);
+          item.setAttribute("name", item.innerText);
+        },
+        filter: function (textNode: any, foundTerm: string, totalCounter: any) {
+          return true; // must return either true or false
+        },
+        done() {
+          // console.log("Done mark addresses");
+        },
+      });
+    })();
   }
 
   window.onload = () => {
