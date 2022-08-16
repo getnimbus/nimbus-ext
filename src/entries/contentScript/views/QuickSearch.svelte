9881605453ed3b131cdf5a4e23f1c322d89c104a
esp-binhng98 <binh.nguyen@eastplayers.io>
2022-08-16 17:44:17 +0700

    done UI sidebar

--- src/entries/contentScript/views/QuickSearch.svelte
+++ src/entries/contentScript/views/QuickSearch.svelte
@@ -2,6 +2,7 @@
 
 <script>
   import { onMount } from "svelte";
+  import { fly } from "svelte/transition";
   import { coinGeko } from "../views/network";
   import { escapeRegex } from "../views/utils";
   import { get } from "lodash";
@@ -42,6 +43,8 @@
 
   let coinList = [];
   let isChangeURL = false;
+  let isShowSideBar = false;
+  let search = "";
 
   onMount(() => {
     getCoinList();
@@ -100,8 +103,76 @@
     }
   });
   observer.observe(document, { subtree: true, childList: true });
+
+  $: {
+    if (!isChangeURL) {
+      isShowSideBar = false;
+    }
+  }
 </script>
 
 {#if isChangeURL}
-  <div class="text-lg text-red-500 fixed top-0 right-0 z-10">hello world</div>
+  <div
+    style="z-index: 999"
+    on:click={() => (isShowSideBar = true)}
+    class="fixed top-[140px] right-0 p-2 bg-sky-100 text-sky-400 rounded-tl rounded-bl cursor-pointer flex items-center gap-1"
+  >
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      class="h-5 w-5"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      stroke-width="2"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M11 19l-7-7 7-7m8 14l-7-7 7-7"
+      />
+    </svg>
+    <div class="font-semibold text-lg">Nimbus 🌩</div>
+  </div>
+{/if}
+
+{#if isShowSideBar}
+  <div
+    transition:fly={{ x: 650, opacity: 1 }}
+    style="z-index: 999"
+    class="fixed top-0 right-0 h-full p-4 bg-sky-100 overflow-y-auto w-[25rem] flex flex-col gap-3"
+  >
+    <div
+      class="cursor-pointer text-sky-500 font-semibold"
+      on:click={() => (isShowSideBar = false)}
+    >
+      Close
+    </div>
+
+    <div class="bg-white text-sky-300 p-2 rounded flex items-center gap-1">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        stroke-width="2"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+        />
+      </svg>
+      <input
+        placeholder="Search..."
+        type="text"
+        bind:value={search}
+        class="font-base w-full py-2 border-none focus:outline-none"
+      />
+    </div>
+
+    <div class="text-3xl font-bold">On this page</div>
+
+    <div>token info card</div>
+  </div>
 {/if}
