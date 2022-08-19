217b33c5302cc00f13835d23c81f731cd3010389
esp-binhng98 <binh.nguyen@eastplayers.io>
2022-08-20 01:30:47 +0700

    add debounce and state get respon when search

--- src/entries/contentScript/views/QuickSearch.svelte
+++ src/entries/contentScript/views/QuickSearch.svelte
@@ -15,10 +15,12 @@
   let coinListData;
   let regexToken;
   let selectedTokenData = [];
+  let tokenDataSearch = [];
   let isChangeURL = false;
   let isShowSideBar = false;
   let search = "";
   let currentUrl = window.location.href;
+  let timer;
 
   onMount(() => {
     getConfigPages();
@@ -56,23 +58,8 @@
     } catch (e) {
       console.error(e);
     }
-    selectedTokenData = get(response, "data.coins") || [];
-  };
-
-  const handleDectecUrl = () => {
-    const selectedPageFromCurrentUrl = listPageConfig.find((item) => {
-      return location.hostname === item.hostname;
-    });
-
-    const patternUrl = selectedPageFromCurrentUrl.urlPattern.map((item) => {
-      return new UrlPattern(item.path);
-    });
-
-    const arrayUrlDetected = patternUrl.map((item) => {
-      return item.match(location.pathname);
-    });
-
-    isChangeURL = arrayUrlDetected.some((el) => el !== null);
+    const data = get(response, "data.coins") || [];
+    tokenDataSearch = data && data.slice(0, 5);
   };
 
   const handleGetCoinDataFromPage = () => {
@@ -100,6 +87,22 @@
     ];
   };
 
+  const handleDectecUrl = () => {
+    const selectedPageFromCurrentUrl = listPageConfig.find((item) => {
+      return location.hostname === item.hostname;
+    });
+
+    const patternUrl = selectedPageFromCurrentUrl.urlPattern.map((item) => {
+      return new UrlPattern(item.path);
+    });
+
+    const arrayUrlDetected = patternUrl.map((item) => {
+      return item.match(location.pathname);
+    });
+
+    isChangeURL = arrayUrlDetected.some((el) => el !== null);
+  };
+
   const observer = new MutationObserver((e) => {
     if (window.location.href !== currentUrl) {
       currentUrl = window.location.href;
@@ -111,6 +114,13 @@
   });
   observer.observe(document, { subtree: true, childList: true });
 
+  const debounce = (value) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      search = value;
+    }, 750);
+  };
+
   $: {
     if (!isChangeURL) {
       isShowSideBar = false;
@@ -120,15 +130,8 @@
   }
 
   $: {
-    console.log("isShowSideBar: ", isShowSideBar);
-    console.log("search: ", search);
-    console.log("selectedTokenData: ", selectedTokenData);
-
-    if (search && listPageConfig && coinListData) {
+    if (search) {
       getSearchData(search);
-    }
-    if (!search && listPageConfig && coinListData) {
-      handleGetCoinDataFromPage();
     }
   }
 </script>
@@ -186,17 +189,23 @@
         />
       </svg>
       <input
+        on:keyup={({ target: { value } }) => debounce(value)}
         placeholder="Search..."
         type="text"
-        bind:value={search}
         class="font-base w-full py-2 border-none focus:outline-none"
       />
     </div>
 
     <div class="text-3xl font-bold">On this page</div>
 
-    {#each selectedTokenData as tokenInfo}
-      <native-token-info id={tokenInfo.id} name={tokenInfo.symbol} />
-    {/each}
+    {#if search}
+      {#each tokenDataSearch as tokenInfo}
+        <native-token-info id={tokenInfo.id} name={tokenInfo.symbol} />
+      {/each}
+    {:else}
+      {#each selectedTokenData as tokenInfo}
+        <native-token-info id={tokenInfo.id} name={tokenInfo.symbol} />
+      {/each}
+    {/if}
   </div>
 {/if}
