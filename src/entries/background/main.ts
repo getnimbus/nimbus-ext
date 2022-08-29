54a8a8d66fa3fa70233bae6138cf94d7f5b86b83
esp-binhng98 <binh.nguyen@eastplayers.io>
2022-08-30 01:50:12 +0700

    change chrome to browser

--- src/entries/background/main.ts
+++ src/entries/background/main.ts
@@ -12,9 +12,8 @@ console.log(browser);
 
 browser.commands.onCommand.addListener((command) => {
   console.log(`Command: ${command}`);
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, { action: "toggleSidebar" });
-  });
+  const tabsQuery = browser.tabs.query({ active: true, currentWindow: true })
+  browser.tabs.sendMessage(tabsQuery[0].id, { action: "toggleSidebar" });
 });
 
 const fetchBasicData = async () => {
