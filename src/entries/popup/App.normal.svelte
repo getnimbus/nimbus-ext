10d4e0946eafc2638572cf2996cead9f2a4a3475
esp-binhng98 <binh.nguyen@eastplayers.io>
2022-09-02 12:26:40 +0700

    fix

--- src/entries/popup/App.normal.svelte
+++ src/entries/popup/App.normal.svelte
@@ -1,9 +1,8 @@
 <svelte:options tag="nimbus-popup" />
 
 <script lang="ts">
-  import * as browser from "webextension-polyfill";
-
-  browser.tabs.query({ active: true, currentWindow: true }).then((tab) => {
-    browser.tabs.sendMessage(tab[0].id, { action: "toggleSidebar" });
-  });
 </script>
+
+<div>Hello world</div>
+
+<style></style>
