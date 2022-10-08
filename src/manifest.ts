d8e00cffda7bf8e2b3ce13495b727061275a98ce
Binh Nguyen <binhnguyen@Binhs-MacBook-Pro.local>
2022-10-08 13:48:15 +0700

    update/update-logo

--- src/manifest.ts
+++ src/manifest.ts
@@ -8,16 +8,16 @@ const sharedManifest = {
     },
   ],
   icons: {
-    16: "icons/16.png",
-    19: "icons/19.png",
-    32: "icons/32.png",
-    38: "icons/38.png",
-    48: "icons/48.png",
-    64: "icons/64.png",
-    96: "icons/96.png",
-    128: "icons/128.png",
-    256: "icons/256.png",
-    512: "icons/512.png",
+    // 16: "icons/16.png",
+    // 19: "icons/19.png",
+    // 32: "icons/32.png",
+    // 38: "icons/38.png",
+    // 48: "icons/48.png",
+    // 64: "icons/64.png",
+    // 96: "icons/96.png",
+    // 128: "icons/128.png",
+    // 256: "icons/256.png",
+    512: "icons/logo-icon.png",
   },
   options_ui: {
     page: "src/entries/options/index.html",
@@ -51,10 +51,10 @@ const sharedManifest = {
 
 const browserAction = {
   default_icon: {
-    16: "icons/16.png",
-    19: "icons/19.png",
-    32: "icons/32.png",
-    38: "icons/38.png",
+    // 16: "icons/16.png",
+    // 19: "icons/19.png",
+    // 32: "icons/32.png",
+    38: "icons/logo-icon.png",
   },
   // default_popup: "src/entries/popup/index.html",
 };
