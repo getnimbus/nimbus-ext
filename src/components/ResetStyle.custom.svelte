79a3e78c0079aa6c0cb29807e5dc07e9bd8f9b2b
Binh Nguyen <binhnguyen@Binhs-MacBook-Pro.local>
2022-12-21 23:08:43 +0700

    feat: add font smoothing

--- src/components/ResetStyle.custom.svelte
+++ src/components/ResetStyle.custom.svelte
@@ -12,6 +12,8 @@
     font-family: ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont,
       Segoe UI, Roboto, Helvetica Neue, Arial, Noto Sans, sans-serif,
       Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji;
+
+    -webkit-font-smoothing: antialiased;
   }
 
   .nimbus-reset-style input {
