ad67d5f969ddf527522c186f1900ef5483a65b2d
binhnguyen2501 <binhq6@gmail.com>
2023-02-05 00:00:52 +0700

    fix: fix import address info

--- src/entries/contentScript/main.ts
+++ src/entries/contentScript/main.ts
@@ -3,6 +3,7 @@ import Mark from "mark.js";
 import "@webcomponents/webcomponentsjs/webcomponents-bundle.js";
 
 import "./views/AddressHighlight.custom.svelte";
+import "./views/AddressInfo.custom.svelte";
 import "./views/AddressSpreadText.custom.svelte";
 import "./views/TrxHighlight.custom.svelte";
 import "./views/QuickSearch.custom.svelte";
@@ -16,9 +17,6 @@ const regexETHAddress = /(\b0x[a-fA-F0-9]{1,40}\b)/g; // TODO: Solana, Near rege
 
 // Firefox `browser.tabs.executeScript()` requires scripts return a primitive value
 (async () => {
-  const coinList: { [key: string]: string | number }[] =
-    (await sendMessage("coinList", { limit: 500 })) || [];
-
   console.info("[Nimbus 🌩] Make Web3 simple for everyone era");
 
   function runMarkElement() {
@@ -66,11 +64,6 @@ const regexETHAddress = /(\b0x[a-fA-F0-9]{1,40}\b)/g; // TODO: Solana, Near rege
         accuracy: "exactly",
         diacritics: false,
         each(item: any) {
-          const selectedItem = coinList.find(
-            (data: { [key: string]: string | number }) =>
-              data.symbol === item.innerText || data.name === item.innerText
-          );
-
           // Inject address as props
           item.setAttribute("address", item.innerText);
         },
