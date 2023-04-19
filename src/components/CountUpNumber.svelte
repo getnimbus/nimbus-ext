035850e8b7ac5ad31a81dded7fd43fbf68acc2d5
binhnguyen2501 <binhq6@gmail.com>
2023-04-19 15:32:11 +0700

    feat: update count up number firttime init

--- src/components/CountUpNumber.svelte
+++ src/components/CountUpNumber.svelte
@@ -1,19 +1,25 @@
 <svelte:options tag="countup-number" />
 
 <script>
+  import { onMount } from "svelte";
   import { CountUp } from "countup.js";
+  import { wait } from "~/entries/background/utils";
   import { formatBigBalance, formatBalance } from "~/utils";
-
-  import tooltip from "~/entries/contentScript/views/tooltip";
 
   export let id;
   export let number;
   export let format = 2;
 
+  let isFirstTime = false;
   let countUp = null;
   let numberFormat = 0;
   let numberSize = "";
   let showTooltip = false;
+
+  onMount(async () => {
+    await wait(3000);
+    isFirstTime = true;
+  });
 
   const easingFn = function (t, b, c, d) {
     var ts = (t /= d) * t;
@@ -21,18 +27,29 @@
     return b + c * (tc + -3 * ts + 3 * t);
   };
 
+  const handleReload = async () => {
+    isFirstTime = false;
+    await wait(3000);
+    isFirstTime = true;
+  };
+
   $: {
     const { number_format, number_size } = formatBigBalance(number);
     numberFormat = number_format;
     numberSize = number_size;
-
     countUp = new CountUp(id, number_format, {
       decimalPlaces: format,
-      duration: 1,
-      startVal: (number_format * 70) / 100,
+      duration: 4,
+      startVal: isFirstTime ? number_format : (number_format * 70) / 100,
       easingFn,
     });
     countUp.start();
+  }
+
+  $: {
+    if (number === 0) {
+      handleReload();
+    }
   }
 </script>
 
