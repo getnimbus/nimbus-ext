ebcc25947af1b9c4f5d23acbcab8d3bc355c1cda
binhnguyen2501 <binhq6@gmail.com>
2023-04-24 21:48:27 +0700

    fix: fix count up number realtime

--- src/components/CountUpNumber.svelte
+++ src/components/CountUpNumber.svelte
@@ -1,7 +1,6 @@
 <script>
   import { onMount } from "svelte";
   import { CountUp } from "countup.js";
-  import { wait } from "~/entries/background/utils";
   import {
     formatBigBalance,
     formatCurrency,
@@ -12,45 +11,42 @@
   export let number;
   export let format = 2;
 
-  let isFirstTime = false;
   let countUp = null;
+  let options = {
+    decimalPlaces: format,
+    duration: 1,
+    startVal: 0,
+  };
   let numberFormat = 0;
   let numberSize = "";
   let showTooltip = false;
+  let numberToCount = 0;
 
-  onMount(async () => {
-    await wait(3000);
-    isFirstTime = true;
+  onMount(() => {
+    countUp = new CountUp(id, numberToCount, options);
+    countUp.start();
   });
-
-  const easingFn = function (t, b, c, d) {
-    var ts = (t /= d) * t;
-    var tc = ts * t;
-    return b + c * (tc + -3 * ts + 3 * t);
-  };
-
-  const handleReload = async () => {
-    isFirstTime = false;
-    await wait(3000);
-    isFirstTime = true;
-  };
 
   $: {
     const { number_format, number_size } = formatBigBalance(number, false);
     numberFormat = number_format;
     numberSize = number_size;
-    countUp = new CountUp(id, number_size === "K" ? number : number_format, {
-      decimalPlaces: format,
-      duration: 4,
-      startVal: isFirstTime ? number_format : (number_format * 70) / 100,
-      easingFn,
-    });
-    countUp.start();
+
+    numberToCount = number_size === "K" ? number : number_format;
+
+    options = {
+      ...options,
+      startVal: (numberToCount * 70) / 100,
+    };
+
+    if (countUp) {
+      countUp.update(numberToCount);
+    }
   }
 
   $: {
-    if (number === 0) {
-      handleReload();
+    if (number === 0 && countUp) {
+      countUp.reset();
     }
   }
 </script>
