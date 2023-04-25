e33a76f1530ef1d4d0dfe458de4614207bcb45e0
Thanh Le <mihthanh27@gmail.com>
2023-04-25 12:01:49 +0700

    Ignore new tab title

--- src/entries/newTab/main.ts
+++ src/entries/newTab/main.ts
@@ -3,9 +3,15 @@ import App from "./App.svelte";
 import * as bg from "../background/main";
 import * as browser from "webextension-polyfill";
 
-// customElements.define("my-element", App);
+browser.storage.sync.get("options").then(async (res) => {
+  const defaultnewtab = await browser.storage.sync.get('defaultnewtab');
+  if (!defaultnewtab?.defaultnewtab) {
+    // Ignore render when default new tab = false
+    return;
+  }
 
-browser.storage.sync.get("options").then((res) => {
+  window.document.title = 'Your personalize Web3 portfolio'
+
   new App({
     target: document.getElementById("app"),
   });
