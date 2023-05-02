d89034671564269b90e42369098373846f6ea941
Thanh Le <mihthanh27@gmail.com>
2023-05-02 21:02:34 +0700

    Prevent open on first time

--- src/store.ts
+++ src/store.ts
@@ -3,12 +3,15 @@ import { writable } from "svelte/store";
 export const isSaveAddressLabel = writable(false);
 export const isOpenReport = writable(false);
 
+let isFirstTime = true;
 isOpenReport.subscribe((value) => {
-  if (APP_TYPE.TYPE === 'WEB') {
+  if (!isFirstTime && APP_TYPE.TYPE === 'WEB') {
     window.postMessage({
       target: 'FeaturebaseWidget',
       data: { action: 'toggleWidget' },
     })
     return;
   }
+
+  isFirstTime = false;
 })
