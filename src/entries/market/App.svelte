4021d1a4fd202b0b8ec4b620857b29bf93bedcd5
Thanh Le <mihthanh27@gmail.com>
2023-05-19 10:04:34 +0700

    Update trade UI

--- src/entries/market/App.svelte
+++ src/entries/market/App.svelte
@@ -148,7 +148,7 @@
       <tbody>
         {#if isLoading}
           <tr>
-            <td colspan="5">
+            <td colspan="6">
               <div class="flex justify-center items-center py-4 px-3">
                 <loading-icon />
               </div>
@@ -156,7 +156,7 @@
           </tr>
         {:else if marketData.length === 0}
           <tr>
-            <td colspan="5">
+            <td colspan="6">
               <div
                 class="flex justify-center items-center py-4 px-3 text-lg text-gray-400"
               >
