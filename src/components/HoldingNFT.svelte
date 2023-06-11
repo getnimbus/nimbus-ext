91004e08dc6761386d7e5f3c6072d04305d7ef81
Thanh Le <mihthanh27@gmail.com>
2023-06-11 09:59:14 +0700

    Add avg cost btc

--- src/components/HoldingNFT.svelte
+++ src/components/HoldingNFT.svelte
@@ -90,9 +90,8 @@
 
   <td class="py-3">
     <div class="text-sm text-[#00000099] font-medium flex justify-end">
-      <TooltipNumber number={data?.totalCostBTC} /> BTC | $<TooltipNumber
-        number={data?.totalCost}
-      />
+      <TooltipNumber number={data?.totalCostBTC} /><span class="mx-1">BTC</span>
+      | $<TooltipNumber number={data?.totalCost} />
     </div>
   </td>
 
