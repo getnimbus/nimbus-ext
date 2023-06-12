425bca8e010cf5b48935270f4b1e8a39efa62981
binhnguyen2501 <binhq6@gmail.com>
2023-06-12 18:22:42 +0700

    hotfix nftcard

--- src/components/NFTCard.svelte
+++ src/components/NFTCard.svelte
@@ -4,7 +4,7 @@
   export let data;
   export let marketPrice;
 
-  $: profitAndLoss = data.est_value * marketPrice - (data.totalCost || 0);
+  $: profitAndLoss = data?.est_value * marketPrice - (data.totalCost || 0);
   $: profitAndLossPercent =
     Math.abs(data.totalCost || 0) === 0
       ? 0
@@ -15,19 +15,19 @@
   class="border border-[#0000000d] rounded-[10px] px-1 pt-1 pb-3 flex flex-col gap-2"
 >
   <div class="rounded-[10px] overflow-hidden h-[270px]">
-    <img src={data.image_url} alt="" class="w-full h-full object-cover" />
+    <img src={data?.image_url} alt="" class="w-full h-full object-cover" />
   </div>
   <div class="flex flex-col gap-1">
-    <div class="text-sm font-semibold">#{data.item_id}</div>
+    <div class="text-sm font-semibold">#{data?.item_id}</div>
     <div class="flex gap-1 items-center">
       <div class="text-sm font-semibold">Inscription</div>
-      <div class="text-sm font-semibold">#{data.inscription_number}</div>
+      <div class="text-sm font-semibold">#{data?.inscription_number}</div>
     </div>
     <div class="text-xs font-normal text-[#616b84] flex gap-1">
       <div>Est. value</div>
       <div>
-        <TooltipNumber number={data.est_value} type="amount" /> BTC | $<TooltipNumber
-          number={data.est_value * marketPrice}
+        <TooltipNumber number={data?.est_value} type="amount" /> BTC | $<TooltipNumber
+          number={data?.est_value * marketPrice}
           type="amount"
         />
       </div>
