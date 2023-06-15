a6c28772ea2aba16faeff7a56435397460ce1144
binhnguyen2501 <binhq6@gmail.com>
2023-06-15 13:57:57 +0700

    convert selectedWallet already save as obj to string

--- src/entries/newTab/main.ts
+++ src/entries/newTab/main.ts
@@ -2,6 +2,8 @@
 import App from "./App.svelte";
 // import * as bg from "../background/main";
 import * as browser from "webextension-polyfill";
+import { isEmpty } from 'lodash';
+import { wallet } from "~/store";
 
 browser.storage.sync.get("options").then(async (res) => {
   const defaultnewtab = await browser.storage.sync.get('defaultnewtab');
@@ -15,6 +17,18 @@ browser.storage.sync.get("options").then(async (res) => {
   }
 
   window.document.title = 'Your personalize Web3 portfolio'
+
+  const selectedWalletRes = await browser.storage.sync.get(
+    "selectedWallet"
+  );
+  if (
+    selectedWalletRes &&
+    !isEmpty(JSON.parse(selectedWalletRes.selectedWallet))
+  ) {
+    wallet.update(
+      (n) => (n = JSON.parse(selectedWalletRes.selectedWallet.value))
+    );
+  }
 
   new App({
     target: document.getElementById("app"),
