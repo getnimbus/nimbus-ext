7295f7501b93aa68fb19877201eeb7363fab5c7e
binhnguyen2501 <binhq6@gmail.com>
2023-07-24 23:19:23 +0700

    fix flow select

--- src/UI/VirtualPortfolio/FormVirtualPortfolio.svelte
+++ src/UI/VirtualPortfolio/FormVirtualPortfolio.svelte
@@ -26,6 +26,7 @@
 
   let selectedTokenList = [];
   let percent = 0;
+  let listTokenCustom = [];
 
   const debounceSearch = (value) => {
     clearTimeout(timerDebounce);
@@ -53,6 +54,7 @@
             name: item[1],
             symbol: item[2],
             logo: `https://s2.coinmarketcap.com/static/img/coins/64x64/${item[0]}.png`,
+            checked: false,
           };
         });
         isLoadingListToken = false;
@@ -69,9 +71,32 @@
           item.name.toLowerCase() === searchValue.toLowerCase() ||
           item.name.toLowerCase().includes(searchValue.toLowerCase())
       )
-    : listToken.slice(0, 500);
+    : listToken.slice(0, 1000);
+
+  $: {
+    if (selectedTokenList) {
+      console.log("selectedTokenList: ", selectedTokenList);
+
+      listTokenCustom = listToken.filter((item) => {
+        return selectedTokenList.some(
+          (selectedToken) => Number(selectedToken) === Number(item.id)
+        );
+      });
+
+      searchDataResult = searchDataResult.map((item) => {
+        const isSelected = listTokenCustom.some(
+          (selectedToken) => selectedToken.id === item.id
+        );
+        return {
+          ...item,
+          checked: isSelected,
+        };
+      });
+    }
+  }
 
   $: console.log("selectedTokenList: ", selectedTokenList);
+  $: console.log("listTokenCustom: ", listTokenCustom);
 </script>
 
 <div class="flex flex-col gap-5">
@@ -186,7 +211,17 @@
                         <input
                           type="checkbox"
                           value={data.id}
-                          bind:group={selectedTokenList}
+                          checked={data.checked}
+                          on:change={(e) => {
+                            const { value, checked } = e.target;
+                            if (checked) {
+                              selectedTokenList = [...selectedTokenList, value];
+                            } else {
+                              selectedTokenList = selectedTokenList.filter(
+                                (item) => item !== value
+                              );
+                            }
+                          }}
                           class="cursor-pointer relative xl:w-4 xl:h-4 w-6 h-6 appearance-none rounded-[0.25rem] border outline-none before:pointer-events-none before:absolute before:h-[0.875rem] before:w-[0.875rem] before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-[0px_0px_0px_13px_transparent] before:content-[''] checked:border-primary checked:bg-primary checked:before:opacity-[0.16] checked:after:absolute checked:after:-mt-px checked:after:ml-[0.25rem] checked:after:block checked:after:h-[0.8125rem] checked:after:w-[0.375rem] checked:after:rotate-45 checked:after:border-[0.125rem] checked:after:border-l-0 checked:after:border-t-0 checked:after:border-solid checked:after:border-white checked:after:bg-transparent checked:after:content-[''] hover:cursor-pointer hover:before:opacity-[0.04] hover:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:shadow-none focus:transition-[border-color_0.2s] focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-[0.875rem] focus:after:w-[0.875rem] focus:after:rounded-[0.125rem] focus:after:content-[''] checked:focus:before:scale-100 checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] checked:focus:after:-mt-px checked:focus:after:ml-[0.25rem] checked:focus:after:h-[0.8125rem] checked:focus:after:w-[0.375rem] checked:focus:after:rotate-45 checked:focus:after:rounded-none checked:focus:after:border-[0.125rem] checked:focus:after:border-l-0 checked:focus:after:border-t-0 checked:focus:after:border-solid checked:focus:after:border-white checked:focus:after:bg-transparent dark:border-neutral-600 dark:checked:border-primary dark:checked:bg-primary dark:focus:before:shadow-[0px_0px_0px_13px_rgba(255,255,255,0.4)] dark:checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca]"
                         />
                       </div>
