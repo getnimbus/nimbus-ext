41e9480c7785eca4ce9b85bdca0e06aac68b63fe
Thanh Le <mihthanh27@gmail.com>
2023-08-08 16:41:12 +0700

    Change query

--- src/layouts/WhalesList.svelte
+++ src/layouts/WhalesList.svelte
@@ -36,7 +36,7 @@
     try {
       isLoading = true;
       const res = await nimbus
-        .get(`/market/portfolio/search?page=${pageValue}`)
+        .get(`/market/portfolio/?page=${pageValue}`)
         .then((response) => response.data);
       whalesData = res;
     } catch (e) {
