4ea1ffeb6dd4099936cf234720aad3e14826b747
Thanh Le <mihthanh27@gmail.com>
2023-08-26 21:33:43 +0700

    Add timeframe warning

--- src/UI/Analytic/Analytic.svelte
+++ src/UI/Analytic/Analytic.svelte
@@ -14,6 +14,7 @@
   import RiskReturnChart from "../AnalyticChart/RiskReturnChart.svelte";
   import Personality from "./Personality.svelte";
   import Compare from "../Portfolio/Compare.svelte";
+  import tooltip from "~/entries/contentScript/views/tooltip";
 
   const navigate = useNavigate();
 
@@ -55,6 +56,28 @@
     <div class="max-w-[2000px] m-auto -mt-32 xl:w-[90%] w-[96%] relative">
       <div class="analytic_container rounded-[20px] xl:p-8 space-y-4">
         <div class="flex items-center justify-end gap-1">
+          Timeframe <span
+            class="text-yellow-400 w-5 h-5 cursor-pointer"
+            use:tooltip={{
+              content: `<tooltip-detail text="Others timeframe will be available soon" />`,
+              allowHTML: true,
+              placement: "top",
+              interactive: true,
+            }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              class="w-5 h-5"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M9.401 3.003c1.155-2 4.043-2 5.197 0l7.355 12.748c1.154 2-.29 4.5-2.599 4.5H4.645c-2.309 0-3.752-2.5-2.598-4.5L9.4 3.003zM12 8.25a.75.75 0 01.75.75v3.75a.75.75 0 01-1.5 0V9a.75.75 0 01.75-.75zm0 8.25a.75.75 0 100-1.5.75.75 0 000 1.5z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          </span>
           <AnimateSharedLayout>
             {#each timeFrame as type}
               <div
--- src/components/TooltipTitle.svelte
+++ src/components/TooltipTitle.svelte
@@ -36,7 +36,7 @@
       />
     {:else}
       <span
-        class={`text-red-500 ${isBigIcon ? "w-4 h-4" : "w-3 h-3"}`}
+        class={`text-yellow-400 ${isBigIcon ? "w-5 h-5" : "w-3 h-3"}`}
         use:tooltip={{
           content: `<tooltip-detail text="${tooltipText}"  link="${link}" />`,
           allowHTML: true,
@@ -44,7 +44,18 @@
           interactive: true,
         }}
       >
-        ⚠︎
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          class="w-5 h-5"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M9.401 3.003c1.155-2 4.043-2 5.197 0l7.355 12.748c1.154 2-.29 4.5-2.599 4.5H4.645c-2.309 0-3.752-2.5-2.598-4.5L9.4 3.003zM12 8.25a.75.75 0 01.75.75v3.75a.75.75 0 01-1.5 0V9a.75.75 0 01.75-.75zm0 8.25a.75.75 0 100-1.5.75.75 0 000 1.5z"
+            clip-rule="evenodd"
+          />
+        </svg>
       </span>
     {/if}
   </span>
