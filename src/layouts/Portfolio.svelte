d8ffd991386b7a08f4140830ccc94e86e0f6dd9e
binhnguyen2501 <binhq6@gmail.com>
2023-09-02 16:19:45 +0700

    add test

--- src/layouts/Portfolio.svelte
+++ src/layouts/Portfolio.svelte
@@ -125,7 +125,6 @@
       dataPieChartOrderBreakdownNft: [],
     },
   };
-
   let selectedTokenHolding = {
     data: [],
     select: [],
@@ -458,10 +457,25 @@
     ],
     queryFn: () => handleGetAllData(typeQueryGetAllData),
     staleTime: Infinity,
-    cacheTime: Infinity,
   });
 
-  $: console.log("queryGetAllData: ", $queryGetAllData);
+  $: queryTest = createQuery({
+    queryKey: [
+      "getAllData",
+      selectedWallet,
+      selectedChain,
+      typeQueryGetAllData,
+    ],
+    queryFn: () => test(typeQueryGetAllData),
+    staleTime: Infinity,
+  });
+
+  const test = (data) => {
+    console.log("HELLO");
+    return "success";
+  };
+
+  $: console.log("queryTest: ", $queryTest);
 
   const getSyncStatus = async () => {
     try {
