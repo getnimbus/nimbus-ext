ff2ac050f83f3d3230e7e7929d385ceb1593de7c
binhnguyen2501 <binhq6@gmail.com>
2023-09-14 01:53:48 +0700

    hotfix demo account

--- vite.config.web.ts
+++ vite.config.web.ts
@@ -55,12 +55,9 @@ export default defineConfig(({ mode }) => {
               "icons": [
                   {
                       "src": "https://lh3.googleusercontent.com/pw/AIL4fc9zAqbIl3Vp_e2vUB6j5gv_UMoING2QAYbQLlJPA3UqayJCqxRzYDYHhzbszSaQmRvqQApZ9a3eBA1M6kIbHKu6bA3EsPmDJgnjK7HvkldSfQUCbT-bO6ZeLNgXcxDLT_xHVlEAQF4v7lnfuzRKv-t3=w509-h509-s-no?authuser=0",
+                      "type": "image/png",
                       "sizes": "509x509",
                       "purpose": "any",
-                  },
-                  {
-                    "src": "https://lh3.googleusercontent.com/pw/AIL4fc96zoITCubpS-OFw0yvx7itZIYOlRvEPiV0htTyhWnxHgJz3XqClMkNjozgOvwHlJIhFtpHLudbklrPrG_iK3BiWnE166jDkYViZffTz7Q6-FZWE6Sv1StSg4CBU8G9a_E4KuL91Tg81FSyGkC6Er0AvgzT9RLRuGNVcZwico8Pzpi_JPhLyoM-AlHnihd1pUZf_lCedgisnXNmE3e66L0NQqWBx10FefJinK6PwtrE-WLLwl5zXxhbZ6ummiLeggCX0oPCBDwr-GQRGLVB-5X-TXIjo3k80x1_NPfTgjJz-p2tq4-Ka4LbhBNoQBgnN3eTNzRDN5MJ1oFT5_MBkw2ZyPvOoThommnGBUdQRl6o5edTtLzgTdTkPXmDoarWmXT2hNYBUhk7O405grHyeFS9EeplD4g9Wv3dGtEV2aXUTZuXxPn1rMi_iSO0-5JO4l-sh4ZlCUiiM-K5hcVKUjFvK52ZU51AAfkd3_lr_rx3aDxyl4n-dhbMInh4T5gfuyGry1oi_DWxyPc7TPiWQp_Nr62zyIYxvr1i0EPnqr4-41oFW4UzPSwil5fIRywgvU-VB7f1-QXshQDLrSBAXv-_Ox9ng4cv5DMcJ-zBXf0GtLob9UQhsJEOvLKkPLM9H8VHSd-EcVCqu6o3h0E7eVoFyPJAkpld3fI6C9dKDAPHZ45pD09WlpYlAHy7RWz7mDJH6pBcrh3zIuAp65A4948q0sDQr_EnnEK-xe8ti82KDEQebVhuBEI2bxCJ8hqP8a1NAdq2gURCCHbZ5uLT1Z1UmNAVIhO10N2xiDcFcgfd_iZjMTYH13hlqwOO476nYBFPjCYoc5l-POFYCHuM3giAr1F-3hpNivWyVTruUuXj9_VY9BdO-rrpEQO80dsHV3tV0yrtaodg2Kffg_6zzYK1OLSf9CZmtlhOJvPgUQJhRcRGvpGWVOZBmwsGZMIoPN-XHD1fPA0atu8UCtt-DGE2sLYsl7Bz=w533-h248-s-no?authuser=0",
-                    "sizes": "533x248",
                   },
               ]
           }, 
