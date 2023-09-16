91c8e3a62fd3e7604f3fc2031b5eeec4de073478
binhnguyen2501 <binhq6@gmail.com>
2023-09-17 00:14:34 +0700

    resize loading

--- src/components/Loading.svelte
+++ src/components/Loading.svelte
@@ -2,7 +2,7 @@
   import { Circle } from "svelte-loading-spinners";
 </script>
 
-<Circle size="30" color="#38bdf8" unit="px" duration="1s" />
+<Circle size="20" color="#38bdf8" unit="px" duration="1s" />
 
 <style>
 </style>
