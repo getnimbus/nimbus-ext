1cd9cb90e9cc52d58af296700059b15b22455a62
huong666 <nguyenxuananhuong541@gmail.com>
2023-09-18 14:31:27 +0700

    fix conflict

--- src/dev-dist/sw.js
+++ src/dev-dist/sw.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 if (!self.define) {
   let e,
     t = {};
@@ -45,3 +46,6 @@ define(["./workbox-fa446783"], function (e) {
       })
     );
 });
+=======
+if(!self.define){let e,t={};const i=(i,r)=>(i=new URL(i+".js",r).href,t[i]||new Promise((t=>{if("document"in self){const e=document.createElement("script");e.src=i,e.onload=t,document.head.appendChild(e)}else e=i,importScripts(i),t()})).then((()=>{let e=t[i];if(!e)throw new Error(`Module ${i} didn’t register its module`);return e})));self.define=(r,o)=>{const n=e||("document"in self?document.currentScript.src:"")||location.href;if(t[n])return;let s={};const l=e=>i(e,n),d={module:{uri:n},exports:s,require:l};t[n]=Promise.all(r.map((e=>d[e]||l(e)))).then((e=>(o(...e),s)))}}define(["./workbox-fa446783"],(function(e){"use strict";self.skipWaiting(),e.clientsClaim(),e.precacheAndRoute([{url:"registerSW.js",revision:"2c81d9b0ad45b1d70d93e4e5be816fdc"},{url:"index.html",revision:"0.6pc1orgfbl"}],{}),e.cleanupOutdatedCaches(),e.registerRoute(new e.NavigationRoute(e.createHandlerBoundToURL("index.html"),{allowlist:[/^\/$/]}))}));
+>>>>>>> 847b28c (add pwa with logo)
--- vite.config.web.ts
+++ vite.config.web.ts
@@ -51,6 +51,7 @@ export default defineConfig(({ mode }) => {
               "short_name": "Nimbus",
               "start_url": "/",
               "display": "standalone",
+              "description": "https://getnimbus.io",
               "icons": [
                   {
                       "src": "/assets/logo-square1.svg",
