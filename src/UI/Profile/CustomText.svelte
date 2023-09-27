e41aa5f636537d7e1a0009f9e3f4098bf0a90cfb
huong666 <nguyenxuananhuong541@gmail.com>
2023-09-27 15:48:05 +0700

    fix custom input

--- src/UI/Profile/CustomText.svelte
+++ src/UI/Profile/CustomText.svelte
@@ -1,10 +1,35 @@
 <script>
+  let isEdit = false;
+
+  let dataInputText = "I believe blockchain isn't only for money";
 </script>
 
 <div
   class="col-span-2 py-2 px-3 h-16 bg-dark-50 text-white rounded-xl flex items-center justify-center"
+  on:click={() => {
+    isEdit = true;
+  }}
 >
-  <div>I believe blockchain isn't only for money</div>
+  {#if !isEdit}
+    <div>{dataInputText}</div>
+  {:else}
+    <form
+      on:submit|preventDefault={(e) => {
+        dataInputText = e.target[0].value;
+        isEdit = false;
+      }}
+      class="w-full"
+    >
+      <input
+        type="text"
+        name="custom text"
+        id="custom-text"
+        placeholder={dataInputText}
+        value={dataInputText}
+        class="bg-dark-50 text-white rounded-lg border-0 outline-none w-full text-center"
+      />
+    </form>
+  {/if}
 </div>
 
 <style></style>
