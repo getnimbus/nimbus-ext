f5d6e09cdb4ad38ed516fa35167bcb4793f94ee9
binhnguyen2501 <binhq6@gmail.com>
2023-09-28 13:54:18 +0700

    add subtitle

--- src/UI/Option/TabAlerts.svelte
+++ src/UI/Option/TabAlerts.svelte
@@ -1,7 +1,12 @@
 <script lang="ts"></script>
 
-<div class="flex flex-col gap-2">
-  <div class="xl:title-3 title-1 mb-2">Alert Settings</div>
+<div class="flex flex-col gap-4">
+  <div class="flex flex-col gap-1 border-b-[1.5px] border_0000000d pb-4">
+    <div class="xl:title-3 title-1 mb-2">Alert Settings</div>
+    <div class="xl:text-base text-xl text-gray-500">
+      Management your setup alerts
+    </div>
+  </div>
   <div>hello</div>
 </div>
 
