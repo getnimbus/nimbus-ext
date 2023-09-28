410ffa16534f30b601c6787d5cefd8376bbfeb29
binhnguyen2501 <binhq6@gmail.com>
2023-09-28 23:38:41 +0700

    update struct profile page

--- src/layouts/Profile.svelte
+++ src/layouts/Profile.svelte
@@ -1,43 +1,36 @@
 <script lang="ts">
-  import { createQueries, createQuery } from "@tanstack/svelte-query";
-  import { empty } from "svelte/internal";
+  import { createQuery } from "@tanstack/svelte-query";
+  import { nimbus } from "~/lib/network";
+  import { chain, typeWallet } from "~/store";
+  import type { HoldingTokenRes } from "~/types/HoldingTokenData";
+  import { shorterAddress } from "~/utils";
+
+  import ErrorBoundary from "~/components/ErrorBoundary.svelte";
+
   import CardInfo from "~/UI/Profile/CardInfo.svelte";
   import CardNft from "~/UI/Profile/CardNFT.svelte";
   import ClosedPositionChart from "~/UI/Profile/ClosedPositionChart.svelte";
   import CustomText from "~/UI/Profile/CustomText.svelte";
   import SocialMedia from "~/UI/Profile/SocialMedia.svelte";
   import SummaryText from "~/UI/Profile/SummaryText.svelte";
-  import Button from "~/components/Button.svelte";
-  import ErrorBoundary from "~/components/ErrorBoundary.svelte";
-  import { i18n } from "~/lib/i18n";
-  import { nimbus } from "~/lib/network";
-  import { chain, typeWallet, wallet } from "~/store";
-  import type { HoldingTokenRes } from "~/types/HoldingTokenData";
-  import { shorterAddress } from "~/utils";
+
   let selectedChain: string = "";
   chain.subscribe((value) => {
     selectedChain = value;
   });
-  let holdingTokenData;
+
   let typeWalletAddress: string = "";
   typeWallet.subscribe((value) => {
     typeWalletAddress = value;
   });
-  let selectedTokenHolding = {
-    data: [],
-    select: [],
-  };
-  let selectedDataPieChart = {};
-  let closedHoldingPosition;
-
-  // this is user address
-  const userAddress = localStorage.getItem("evm_address");
-  // this address is for testing
-  const testAddress = "0x8980dbbe60d92b53b08ff95ea1aaaabb7f665bcb";
 
   const getHoldingToken = async (chain) => {
     const response: HoldingTokenRes = await nimbus
-      .get(`/v2/address/${testAddress}/holding?chain=${chain}`)
+      .get(
+        `/v2/address/${localStorage.getItem(
+          "evm_address"
+        )}/holding?chain=${chain}`
+      )
       .then((response) => response.data);
     return response;
   };
@@ -53,40 +46,41 @@
 
 <ErrorBoundary>
   <div
-    class="max-w-[2000px] m-auto w-[90%] min-h-screen py-8 flex flex-col xl:gap-10 gap-6"
+    class="max-w-[2000px] m-auto xl:w-[90%] w-[90%] py-8 flex flex-col gap-10"
   >
-    <div class="flex flex-col mb-5 gap-14">
-      <div class="font-medium xl:text-5xl text-7xl">My Profile</div>
+    <div class="flex flex-col justify-center gap-2">
+      <div class="xl:text-5xl text-7xl font-semibold">My Profile</div>
+      <div class="xl:text-xl text-3xl">
+        One place that aggregates all your personal information
+      </div>
+    </div>
+    <div
+      class="w-full flex xl:flex-row flex-col border-2 rounded-xl shadow-light-600 shadow-xl min-h-screen py-10 px-10 gap-9"
+    >
       <div
-        class="w-full flex xl:flex-row flex-col border-2 rounded-xl shadow-light-600 shadow-xl min-h-screen py-10 px-10 gap-9"
+        class="xl:w-1/4 w-full flex flex-col gap-3 items-center justify-start"
       >
-        <div
-          class="xl:w-1/3 w-full h-[300px] flex items-center justify-center rounded-xl"
-        >
-          <div
-            class="flex flex-col gap-3 items-center justify-center text-2xl xl:text-base"
-          >
-            <div class=" xl:w-[80px] xl:h-[80px] w-32 h-32">
-              <img
-                src={`/assets/user.png`}
-                alt=""
-                class="object-cover w-full h-full"
-              />
-            </div>
-            {shorterAddress(localStorage.getItem("evm_address") || "")}
-          </div>
+        <div class="xl:w-[80px] xl:h-[80px] w-32 h-32">
+          <img
+            src={`/assets/user.png`}
+            alt=""
+            class="object-cover w-full h-full"
+          />
         </div>
-        <div class="xl:w-2/3 w-[428px] h-full mx-auto">
-          <div class="text-xl xl:3xl font-medium mb-4">My Story</div>
-          <div class="grid xl:grid-cols-4 grid-cols-2 gap-10">
-            <SummaryText userData={$queryTokenHolding.data} />
-            <CardNft />
-            <CardNft />
-            <CustomText />
-            <CardInfo />
-            <SocialMedia />
-            <ClosedPositionChart />
-          </div>
+        <div class="text-2xl xl:text-base font-medium">
+          {shorterAddress(localStorage.getItem("evm_address") || "")}
+        </div>
+      </div>
+      <div class="flex-1 flex flex-col gap-4">
+        <div class="xl:text-3xl text-xl font-medium">My Story</div>
+        <div class="grid xl:grid-cols-4 grid-cols-2 gap-10">
+          <SummaryText userData={$queryTokenHolding.data} />
+          <CardNft />
+          <CardNft />
+          <CustomText />
+          <CardInfo />
+          <SocialMedia />
+          <ClosedPositionChart />
         </div>
       </div>
     </div>
