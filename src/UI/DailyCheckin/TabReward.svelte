595c023fec309f6c0fb0dbc3f18685e6109968ce
huong666 <nguyenxuananhuong541@gmail.com>
2023-10-12 12:02:28 +0700

    update your gift

--- src/UI/DailyCheckin/TabReward.svelte
+++ src/UI/DailyCheckin/TabReward.svelte
@@ -173,6 +173,73 @@
           {/each}
         {:else if selectedTypePerformance === "yourGift"}
           <!-- Your gift - ownRewards -->
+          {#each $queryReward?.data?.ownRewards || [] as item}
+            <!-- a card  -->
+            <div
+              class={`flex flex-col rounded-2xl ${
+                darkMode ? "bg-[#212121]" : "border"
+              } `}
+            >
+              <div
+                class="grid grid-cols-3 items-center xl:gap-5 gap-20 px-8 py-5 border-b-2 border-dashed"
+              >
+                <div
+                  class={`xl:w-auto xl:h-auto h-[200px] col-span-1 rounded-2xl px-2 flex flex-col items-center justify-center ${
+                    darkMode ? "bg-gray-200" : "bg-white"
+                  }`}
+                >
+                  <img
+                    src={item.logo}
+                    alt=""
+                    class="xl:object-contain m-auto"
+                  />
+                </div>
+                <div class="flex flex-col gap-2 col-span-2">
+                  <div
+                    class="flex gap-1 items-center font-medium xl:text-2xl text-3xl text-[#ffb800]"
+                  >
+                    <svg
+                      width="26"
+                      height="26"
+                      viewBox="0 0 16 16"
+                      fill="#ffb800"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        clip-rule="evenodd"
+                        d="M14.6629 3.5843C14.7217 3.57771 14.7811 3.58901 14.8339 3.61685C14.8867 3.64495 14.9305 3.68852 14.9599 3.74223C14.9893 3.79594 15.0031 3.85745 14.9994 3.91919L14.4836 12.7921H1.51642L1.00059 3.91919C0.996892 3.85745 1.01055 3.79592 1.0399 3.74216C1.06924 3.68841 1.11299 3.64476 1.16578 3.6166C1.21856 3.58843 1.27808 3.57697 1.33702 3.58362C1.39596 3.59026 1.45175 3.61473 1.49755 3.65401L4.60499 6.30708L7.76082 2.11502C7.79036 2.07895 7.82704 2.04999 7.86833 2.03014C7.90962 2.01028 7.95455 2 8.00001 2C8.04548 2 8.0904 2.01028 8.1317 2.03014C8.17299 2.04999 8.20967 2.07895 8.23921 2.11502L11.395 6.30708L14.5025 3.65401C14.5484 3.61511 14.6041 3.5909 14.6629 3.5843ZM1.55334 13.4273L1.55781 13.5041C1.577 13.827 1.71333 14.1301 1.93906 14.3518C2.1648 14.5735 2.46298 14.6971 2.77297 14.6976H13.2271C13.537 14.6971 13.8352 14.5735 14.061 14.3518C14.2867 14.1301 14.423 13.827 14.4422 13.5041L14.4467 13.4273H1.55334Z"
+                        fill="#ffb800"
+                      />
+                    </svg>
+                    {item.campaignName}
+                  </div>
+                  <div class="xl:text-3xl text-4xl uppercase">{item.title}</div>
+                  <div class="xl:text-base text-xl">{item.description}</div>
+                </div>
+              </div>
+              <div
+                class="grid grid-cols-3 items-center xl:gap-5 gap-20 px-8 py-5"
+              >
+                <div class="col-span-1 xl:text-base text-xl">
+                  {item.remains} left
+                </div>
+                <div
+                  class="col-span-2 py-[10px] bg-[#1e96fc] rounded-xl text-white"
+                >
+                  <div
+                    class="flex justify-center items-center gap-5 xl:text-base text-xl"
+                  >
+                    <span class="flex items-center gap-1">
+                      <img src={imgGold} alt="" />
+                      <span>{item.cost}</span>
+                    </span>
+                    <span class="text-left">Redeem</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          {/each}
         {/if}
       </div>
     </div>
