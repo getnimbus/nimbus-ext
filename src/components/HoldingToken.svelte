aeec7053a5044897fe856b3176a9eee3065b149e
binhnguyen2501 <binhq6@gmail.com>
2023-10-16 22:34:13 +0700

    feat/reopen-token-detail

--- src/components/HoldingToken.svelte
+++ src/components/HoldingToken.svelte
@@ -23,6 +23,7 @@
   import VaultTable from "~/UI/Portfolio/VaultTable.svelte";
   import Button from "./Button.svelte";
   import Copy from "~/components/Copy.svelte";
+  import OverlaySidebar from "./OverlaySidebar.svelte";
 
   import TrendUp from "~/assets/trend-up.svg";
   import TrendDown from "~/assets/trend-down.svg";
@@ -79,6 +80,8 @@
 
   let isOpenTokenInfoBundle = false;
   let showTokenInfoBundle = false;
+
+  let showSideTokenDetail = false;
 
   const trigger = () => {
     showToast = true;
@@ -853,26 +856,20 @@
     </div>
   </td>
 
-  {#if typeWalletAddress === "BUNDLE"}
-    <td
-      class={`py-3 w-10 ${
-        darkMode ? "group-hover:bg-[#000]" : "group-hover:bg-gray-100"
-      }`}
-    >
-      <!-- {#if clickable}
-        <div class="flex justify-center">
-          <div
-            use:tooltip={{
-              content: `<tooltip-detail text="Show detail" />`,
-              allowHTML: true,
-              placement: "top",
-            }}
-          >
-            <img src={Chart} alt="" width={14} height={14} />
-          </div>
-        </div>
-      {/if} -->
-      <div class="flex justify-center">
+  <td
+    class={`py-3 xl:w-12 w-32 h-full flex justify-center items-center xl:gap-3 gap-6 ${
+      darkMode ? "group-hover:bg-[#000]" : "group-hover:bg-gray-100"
+    }`}
+  >
+    {#if typeWalletAddress === "BUNDLE"}
+      <div
+        class="flex justify-center"
+        use:tooltip={{
+          content: `<tooltip-detail text="Show bundles detail" />`,
+          allowHTML: true,
+          placement: "top",
+        }}
+      >
         <div
           class="xl:block hidden cursor-pointer transform rotate-180 xl:w-3 xl:h-3 w-5 h-5"
           class:rotate-0={isOpenTokenInfoBundle}
@@ -887,7 +884,7 @@
           >
             <path
               d="M10 8.36365L6 4.00001L2 8.36365"
-              stroke={darkMode ? "white" : "black"}
+              stroke={darkMode ? "white" : "#00000080"}
               stroke-width="2"
               stroke-linecap="round"
               stroke-linejoin="round"
@@ -905,14 +902,78 @@
             xmlns="http://www.w3.org/2000/svg"
           >
             <path
-              fill={darkMode ? "white" : "black"}
+              fill={darkMode ? "white" : "#00000080"}
               d="M3.5 4A1.5 1.5 0 0 0 2 5.5v2A1.5 1.5 0 0 0 3.5 9h2A1.5 1.5 0 0 0 7 7.5v-2A1.5 1.5 0 0 0 5.5 4h-2ZM3 5.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2ZM9.5 5a.5.5 0 0 0 0 1h8a.5.5 0 0 0 0-1h-8Zm0 2a.5.5 0 0 0 0 1h6a.5.5 0 0 0 0-1h-6Zm-6 4A1.5 1.5 0 0 0 2 12.5v2A1.5 1.5 0 0 0 3.5 16h2A1.5 1.5 0 0 0 7 14.5v-2A1.5 1.5 0 0 0 5.5 11h-2ZM3 12.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2Zm6.5-.5a.5.5 0 0 0 0 1h8a.5.5 0 0 0 0-1h-8Zm0 2a.5.5 0 0 0 0 1h6a.5.5 0 0 0 0-1h-6Z"
             />
           </svg>
         </div>
       </div>
-    </td>
-  {/if}
+    {/if}
+    <div
+      class="flex justify-center cursor-pointer"
+      on:click={() => (showSideTokenDetail = true)}
+    >
+      <div
+        use:tooltip={{
+          content: `<tooltip-detail text="Show token detail" />`,
+          allowHTML: true,
+          placement: "top",
+        }}
+        class="xl:w-[14px] xl:h-[14px] w-[26px] h-[26px]"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+          version="1.1"
+          viewBox="0 0 256 256"
+          xml:space="preserve"
+        >
+          <defs />
+          <g
+            style="stroke: none; stroke-width: 0; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill-rule: nonzero; opacity: 1;"
+            fill={darkMode ? "white" : "#00000080"}
+            transform="translate(1.4065934065934016 1.4065934065934016) scale(2.81 2.81)"
+          >
+            <path
+              d="M 87.994 0 H 69.342 c -1.787 0 -2.682 2.16 -1.418 3.424 l 5.795 5.795 l -33.82 33.82 L 28.056 31.196 l -3.174 -3.174 c -1.074 -1.074 -2.815 -1.074 -3.889 0 L 0.805 48.209 c -1.074 1.074 -1.074 2.815 0 3.889 l 3.174 3.174 c 1.074 1.074 2.815 1.074 3.889 0 l 15.069 -15.069 l 14.994 14.994 c 1.074 1.074 2.815 1.074 3.889 0 l 1.614 -1.614 c 0.083 -0.066 0.17 -0.125 0.247 -0.202 l 37.1 -37.1 l 5.795 5.795 C 87.84 23.34 90 22.445 90 20.658 V 2.006 C 90 0.898 89.102 0 87.994 0 z"
+              style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill-rule: nonzero; opacity: 1;"
+              transform=" matrix(1 0 0 1 0 0) "
+              fill={darkMode ? "white" : "#00000080"}
+              stroke-linecap="round"
+            />
+            <path
+              d="M 65.626 37.8 v 49.45 c 0 1.519 1.231 2.75 2.75 2.75 h 8.782 c 1.519 0 2.75 -1.231 2.75 -2.75 V 23.518 L 65.626 37.8 z"
+              style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill-rule: nonzero; opacity: 1;"
+              fill={darkMode ? "white" : "#00000080"}
+              transform=" matrix(1 0 0 1 0 0) "
+              stroke-linecap="round"
+            />
+            <path
+              d="M 47.115 56.312 V 87.25 c 0 1.519 1.231 2.75 2.75 2.75 h 8.782 c 1.519 0 2.75 -1.231 2.75 -2.75 V 42.03 L 47.115 56.312 z"
+              style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill-rule: nonzero; opacity: 1;"
+              fill={darkMode ? "white" : "#00000080"}
+              transform=" matrix(1 0 0 1 0 0) "
+              stroke-linecap="round"
+            />
+            <path
+              d="M 39.876 60.503 c -1.937 0 -3.757 -0.754 -5.127 -2.124 l -6.146 -6.145 V 87.25 c 0 1.519 1.231 2.75 2.75 2.75 h 8.782 c 1.519 0 2.75 -1.231 2.75 -2.75 V 59.844 C 41.952 60.271 40.933 60.503 39.876 60.503 z"
+              style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill-rule: nonzero; opacity: 1;"
+              fill={darkMode ? "white" : "#00000080"}
+              transform=" matrix(1 0 0 1 0 0) "
+              stroke-linecap="round"
+            />
+            <path
+              d="M 22.937 46.567 L 11.051 58.453 c -0.298 0.298 -0.621 0.562 -0.959 0.8 V 87.25 c 0 1.519 1.231 2.75 2.75 2.75 h 8.782 c 1.519 0 2.75 -1.231 2.75 -2.75 V 48.004 L 22.937 46.567 z"
+              style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill-rule: nonzero; opacity: 1;"
+              fill={darkMode ? "white" : "#00000080"}
+              transform=" matrix(1 0 0 1 0 0) "
+              stroke-linecap="round"
+            />
+          </g>
+        </svg>
+      </div>
+    </div>
+  </td>
 </tr>
 
 {#if isOpenTokenInfoBundle}
@@ -1336,6 +1397,32 @@
     </form>
   </div>
 </AppOverlay>
+
+<!-- Sidebar Token Detail -->
+<OverlaySidebar
+  isOpen={showSideTokenDetail}
+  on:close={() => {
+    showSideTokenDetail = false;
+  }}
+>
+  <div class="flex flex-col gap-6 p-6">
+    <div class="flex justify-between items-start">
+      <div
+        class="xl:text-5xl text-6xl text-gray-500 cursor-pointer"
+        on:click|stopPropagation={() => {
+          showSideTokenDetail = false;
+        }}
+      >
+        &times;
+      </div>
+      <div class="flex flex-col items-end">
+        <div>token logo and name and symbol</div>
+        <div>token price</div>
+      </div>
+    </div>
+    <div>hello world</div>
+  </div>
+</OverlaySidebar>
 
 {#if showToast}
   <div class="fixed z-30 w-full top-3 right-3">
