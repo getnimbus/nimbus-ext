d5e711244416cf5c412957d522ee5115260d88e7
binhnguyen2501 <binhq6@gmail.com>
2023-10-25 13:54:13 +0700

    hotfix add copy contract address token detail holding

--- src/components/HoldingToken.svelte
+++ src/components/HoldingToken.svelte
@@ -13,6 +13,8 @@
   import dayjs from "dayjs";
   import { nimbus } from "~/lib/network";
   import { i18n } from "~/lib/i18n";
+  import CopyToClipboard from "svelte-copy-to-clipboard";
+  import { wait } from "../entries/background/utils";
 
   import "~/components/Tooltip.custom.svelte";
   import tooltip from "~/entries/contentScript/views/tooltip";
@@ -53,6 +55,7 @@
 
   let showSideTokenDetail = false;
   let selectedTokenDetail = {};
+  let isCopied = false;
 
   const trigger = () => {
     showToast = true;
@@ -1472,6 +1475,54 @@
                     </div>
                   {/if}
                 </div>
+                <CopyToClipboard
+                  text={selectedTokenDetail?.contractAddress}
+                  let:copy
+                  on:copy={async () => {
+                    isCopied = true;
+                    await wait(1000);
+                    isCopied = false;
+                  }}
+                >
+                  <div class="cursor-pointer" on:click={copy}>
+                    {#if isCopied}
+                      <svg
+                        width={20}
+                        height={20}
+                        id="Layer_1"
+                        data-name="Layer 1"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 122.88 74.46"
+                        fill={$isDarkMode ? "#d1d5db" : "#00000080"}
+                        ><path
+                          fill-rule="evenodd"
+                          d="M1.87,47.2a6.33,6.33,0,1,1,8.92-9c8.88,8.85,17.53,17.66,26.53,26.45l-3.76,4.45-.35.37a6.33,6.33,0,0,1-8.95,0L1.87,47.2ZM30,43.55a6.33,6.33,0,1,1,8.82-9.07l25,24.38L111.64,2.29c5.37-6.35,15,1.84,9.66,8.18L69.07,72.22l-.3.33a6.33,6.33,0,0,1-8.95.12L30,43.55Zm28.76-4.21-.31.33-9.07-8.85L71.67,4.42c5.37-6.35,15,1.83,9.67,8.18L58.74,39.34Z"
+                        /></svg
+                      >
+                    {:else}
+                      <svg
+                        width={20}
+                        height={20}
+                        viewBox="0 0 12 11"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M8.1875 3.3125H10.6875V10.1875H3.8125V7.6875"
+                          stroke={$isDarkMode ? "#d1d5db" : "#00000080"}
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                        />
+                        <path
+                          d="M8.1875 0.8125H1.3125V7.6875H8.1875V0.8125Z"
+                          stroke={$isDarkMode ? "#d1d5db" : "#00000080"}
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                        />
+                      </svg>
+                    {/if}
+                  </div>
+                </CopyToClipboard>
               </div>
             </div>
           </div>
