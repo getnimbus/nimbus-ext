c7a5e60a667fd48f053b3c65ed281c4d34ba5ba5
binhnguyen2501 <binhq6@gmail.com>
2023-11-10 19:33:03 +0700

    update UI history table

--- src/UI/NFTDetail/NFTHistoryItem.svelte
+++ src/UI/NFTDetail/NFTHistoryItem.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-  import { isDarkMode, typeWallet } from "~/store";
-  import { detectedChain } from "~/utils";
+  import { isDarkMode, typeWallet, wallet } from "~/store";
+  import { detectedChain, linkExplorer } from "~/utils";
   import dayjs from "dayjs";
   import "dayjs/locale/en";
   import "dayjs/locale/vi";
@@ -8,8 +8,99 @@
   dayjs.extend(relativeTime);
 
   import TooltipNumber from "~/components/TooltipNumber.svelte";
+
+  export let data;
+
+  $: console.log("data: ", data);
+
+  $: withinLast24Hours = dayjs().diff(dayjs(data?.timestamp), "hour");
 </script>
 
-<div>hello world</div>
+<tr class="group transition-all">
+  <td
+    class={`pl-3 py-3 xl:static xl:bg-transparent sticky left-0 z-10 w-[100px] ${
+      $isDarkMode
+        ? "bg-[#131313] group-hover:bg-[#000]"
+        : "bg-white group-hover:bg-gray-100"
+    }`}
+  >
+    <div class="xl:text-sm text-2xl font-medium flex justify-start">
+      {data?.tokenId}
+    </div>
+  </td>
+
+  <td
+    class={`py-3 ${
+      $isDarkMode ? "group-hover:bg-[#000]" : "group-hover:bg-gray-100"
+    }`}
+  >
+    <div class="xl:text-sm text-2xl font-medium flex justify-end">
+      hello world
+    </div>
+  </td>
+
+  <td
+    class={`py-3 ${
+      $isDarkMode ? "group-hover:bg-[#000]" : "group-hover:bg-gray-100"
+    }`}
+  >
+    <div class="xl:text-sm text-2xl text_00000099 font-medium flex justify-end">
+      <div
+        class="w-max px-2 py-1 text_27326F font-normal bg-[#6AC7F533] rounded-[5px] capitalize"
+      >
+        {data?.owner?.toLowerCase() === data?.fromAddress?.toLowerCase()
+          ? "Sell"
+          : "Buy"}
+      </div>
+    </div>
+  </td>
+
+  <td
+    class={`py-3 ${
+      $isDarkMode ? "group-hover:bg-[#000]" : "group-hover:bg-gray-100"
+    }`}
+  >
+    <div class="xl:text-sm text-2xl text_00000099 font-medium flex justify-end">
+      {data?.price}
+      {data?.chain} | ${data?.priceUst}
+    </div>
+  </td>
+
+  <td
+    class={`py-3 ${
+      $isDarkMode ? "group-hover:bg-[#000]" : "group-hover:bg-gray-100"
+    }`}
+  >
+    <div class="xl:text-sm text-2xl text_00000099 font-medium text-right">
+      {withinLast24Hours < 24 && withinLast24Hours > 0
+        ? dayjs(data?.timestamp).fromNow()
+        : dayjs(data?.timestamp).format("YYYY-MM-DD")}
+    </div>
+  </td>
+
+  <td
+    class={`py-3 w-10 ${
+      $isDarkMode ? "group-hover:bg-[#000]" : "group-hover:bg-gray-100"
+    }`}
+  >
+    <div class="flex items-center justify-center">
+      <a
+        href={data.transactionHash
+          ? linkExplorer(data.chain, data.transactionHash).trx
+          : ""}
+        target="_blank"
+        class="cursor-pointer"
+      >
+        <img
+          src={detectedChain(data?.chain)}
+          alt=""
+          width="22"
+          height="22"
+          class="rounded-full"
+        />
+      </a>
+    </div>
+  </td>
+</tr>
 
 <style></style>
--- src/UI/NFTDetail/NFTTradeHistory.svelte
+++ src/UI/NFTDetail/NFTTradeHistory.svelte
@@ -15,7 +15,106 @@
     return response?.data;
   };
 
-  let dataHistoryNftDetail = [];
+  let dataHistoryNftDetail = [
+    {
+      id: "ab557a32-9713-4e05-9636-79e2db71cb15",
+      transactionHash:
+        "0x044951039281ec412dcc36b8bf95a1eddb7a5bd08b017d8055574ff454eefda3",
+      owner: "0x8bc924a4c07b4369a58cf35f6dc3ba16c41fc999",
+      fromAddress: "0x9ada4d7c6c05dd2eca87ce866c46793a4dd4a93f",
+      toAddress: "0x8bc924a4c07b4369a58cf35f6dc3ba16c41fc999",
+      chain: "ETH",
+      address: "0x6a4732395e70634dc1a5d4e5620340c311f08c01",
+      tokenId: "51",
+      price: 0.925,
+      priceUst: 1684.37,
+      timestamp: "2023-08-04T20:46:47.000Z",
+    },
+    {
+      id: "01016da7-cf38-45b0-8fe3-13d6e20e1ba3",
+      transactionHash:
+        "0x5e88e516939a94e282bd522bd8e3c63247a08eccce14b01ac0c6325a0d0c97ee",
+      owner: "0x8bc924a4c07b4369a58cf35f6dc3ba16c41fc999",
+      fromAddress: "0x3fb65feeab83bf60b0d1ffbc4217d2d97a35c8d4",
+      toAddress: "0x8bc924a4c07b4369a58cf35f6dc3ba16c41fc999",
+      chain: "ETH",
+      address: "0x3f0b6fe90aa17c59d200fa4baedc8935cea24f7c",
+      tokenId: "17",
+      price: 0.1595,
+      priceUst: 263.62,
+      timestamp: "2023-08-24T20:20:35.000Z",
+    },
+    {
+      id: "ab1f5aa7-d2f7-4491-a7db-7c6e33d5fc75",
+      transactionHash:
+        "0x5e88e516939a94e282bd522bd8e3c63247a08eccce14b01ac0c6325a0d0c97ee",
+      owner: "0x8bc924a4c07b4369a58cf35f6dc3ba16c41fc999",
+      fromAddress: "0x3fb65feeab83bf60b0d1ffbc4217d2d97a35c8d4",
+      toAddress: "0x8bc924a4c07b4369a58cf35f6dc3ba16c41fc999",
+      chain: "ETH",
+      address: "0x3f0b6fe90aa17c59d200fa4baedc8935cea24f7c",
+      tokenId: "305",
+      price: 0.16,
+      priceUst: 264.45,
+      timestamp: "2023-08-24T20:20:35.000Z",
+    },
+    {
+      id: "3165aabc-35f2-416b-b458-e7aaf6f45416",
+      transactionHash:
+        "0x5e88e516939a94e282bd522bd8e3c63247a08eccce14b01ac0c6325a0d0c97ee",
+      owner: "0x8bc924a4c07b4369a58cf35f6dc3ba16c41fc999",
+      fromAddress: "0x3fb65feeab83bf60b0d1ffbc4217d2d97a35c8d4",
+      toAddress: "0x8bc924a4c07b4369a58cf35f6dc3ba16c41fc999",
+      chain: "ETH",
+      address: "0x3f0b6fe90aa17c59d200fa4baedc8935cea24f7c",
+      tokenId: "307",
+      price: 0.16,
+      priceUst: 264.45,
+      timestamp: "2023-08-24T20:20:35.000Z",
+    },
+    {
+      id: "d7fa242a-687a-4d8b-96f7-e05cc1f2b4fe",
+      transactionHash:
+        "0x6f39cdc8d7a2d552f0f167ab544d3a8024247b51f27dfbc8d4bea06fb4df2651",
+      owner: "0x8bc924a4c07b4369a58cf35f6dc3ba16c41fc999",
+      fromAddress: "0xe2591bd3d77e4ab6a477b8fc776a36f7aee13a1d",
+      toAddress: "0x8bc924a4c07b4369a58cf35f6dc3ba16c41fc999",
+      chain: "ETH",
+      address: "0x3f0b6fe90aa17c59d200fa4baedc8935cea24f7c",
+      tokenId: "107",
+      price: 0.128,
+      priceUst: 211.31,
+      timestamp: "2023-08-24T21:11:23.000Z",
+    },
+    {
+      id: "71a5e4f9-ccd3-4981-b208-2345eb5687d3",
+      transactionHash:
+        "0x6f39cdc8d7a2d552f0f167ab544d3a8024247b51f27dfbc8d4bea06fb4df2651",
+      owner: "0x8bc924a4c07b4369a58cf35f6dc3ba16c41fc999",
+      fromAddress: "0xe2591bd3d77e4ab6a477b8fc776a36f7aee13a1d",
+      toAddress: "0x8bc924a4c07b4369a58cf35f6dc3ba16c41fc999",
+      chain: "ETH",
+      address: "0x3f0b6fe90aa17c59d200fa4baedc8935cea24f7c",
+      tokenId: "318",
+      price: 0.1295,
+      priceUst: 213.78,
+      timestamp: "2023-08-24T21:11:23.000Z",
+    },
+    {
+      id: "66832b91-45dd-4516-97ce-008e3abbdeb6",
+      transactionHash:
+        "0x6f39cdc8d7a2d552f0f167ab544d3a8024247b51f27dfbc8d4bea06fb4df2651",
+      owner: "0x8bc924a4c07b4369a58cf35f6dc3ba16c41fc999",
+      fromAddress: "0xe2591bd3d77e4ab6a477b8fc776a36f7aee13a1d",
+      toAddress: "0x8bc924a4c07b4369a58cf35f6dc3ba16c41fc999",
+      chain: "ETH",
+      address: "0x3f0b6fe90aa17c59d200fa4baedc8935cea24f7c",
+      tokenId: "415",
+      price: 0.1295,
+      priceUst: 213.78,
+      timestamp: "2023-08-24T21:11:23.000Z",
+    },
+  ];
 
   // $: queryHistoryNftDetail = createQuery({
   //   queryKey: ["nft-trade-history", selectedNftCollectionId, $wallet, selectedNftCollectionChain],
@@ -61,7 +160,7 @@
             class="pl-3 py-3 rounded-tl-[10px] xl:static xl:bg-transparent sticky left-0 z-10 bg_f4f5f8 w-[100px]"
           >
             <div class="text-left xl:text-xs text-xl uppercase font-medium">
-              Amount
+              Token ID
             </div>
           </th>
 
@@ -73,33 +172,29 @@
 
           <th class="py-3">
             <div class="text-right xl:text-xs text-xl uppercase font-medium">
+              Type
+            </div>
+          </th>
+
+          <th class="py-3">
+            <div class="text-right xl:text-xs text-xl uppercase font-medium">
               Price
             </div>
           </th>
 
-          <th
-            class={`py-3 rounded-tr-[10px] ${
-              $typeWallet === "SOL" ||
-              $typeWallet === "EVM" ||
-              $typeWallet === "BUNDLE"
-                ? ""
-                : "pr-3"
-            }`}
-          >
+          <th class="py-3 rounded-tr-[10px]">
             <div class="text-right xl:text-xs text-xl uppercase font-medium">
               Time
             </div>
           </th>
 
-          {#if $typeWallet === "SOL" || $typeWallet === "EVM" || $typeWallet === "BUNDLE"}
-            <th class="py-3 w-10" />
-          {/if}
+          <th class="py-3 w-10" />
         </tr>
       </thead>
       <!-- {#if $queryHistoryNftDetail.isFetching}
         <tbody>
           <tr>
-            <td colspan="4">
+            <td colspan="7">
               <div class="flex justify-center items-center h-full py-3 px-3">
                 <Loading />
               </div>
@@ -110,7 +205,7 @@
         <tbody>
           {#if dataHistoryNftDetail && dataHistoryNftDetail?.length === 0}
             <tr>
-              <td colspan="4">
+              <td colspan="7">
                 <div
                   class="flex justify-center items-center h-full py-3 px-3 xl:text-lg text-xl text-gray-400"
                 >
@@ -128,6 +223,11 @@
           {/if}
         </tbody>
       {/if} -->
+      <tbody>
+        {#each dataHistoryNftDetail as item}
+          <NFTHistoryItem data={item} />
+        {/each}
+      </tbody>
     </table>
   </div>
 </div>
