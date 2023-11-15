6980b209001904573b5c40a57dc2ad1b1d05282e
binhnguyen2501 <binhq6@gmail.com>
2023-11-16 00:17:24 +0700

    update sort icon

--- src/layouts/WhalesList.svelte
+++ src/layouts/WhalesList.svelte
@@ -46,51 +46,6 @@
   let sortMaxDrawDown = "default";
   let sortVolatility = "default";
 
-  $: iconSorting = (sortType) => {
-    return `  <svg
-                    height="0.9rem"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <g id="SVGRepo_bgCarrier" stroke-width="0" /><g
-                      id="SVGRepo_tracerCarrier"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    />
-                    <g id="SVGRepo_iconCarrier">
-                      <path
-                        d="M16.0686 15H7.9313C7.32548 15 7.02257 15 6.88231 15.1198C6.76061 15.2238 6.69602 15.3797 6.70858 15.5393C6.72305 15.7232 6.93724 15.9374 7.36561 16.3657L11.4342 20.4344C11.6323 20.6324 11.7313 20.7314 11.8454 20.7685C11.9458 20.8011 12.054 20.8011 12.1544 20.7685C12.2686 20.7314 12.3676 20.6324 12.5656 20.4344L16.6342 16.3657C17.0626 15.9374 17.2768 15.7232 17.2913 15.5393C17.3038 15.3797 17.2392 15.2238 17.1175 15.1198C16.9773 15 16.6744 15 16.0686 15Z"
-                        stroke="${$isDarkMode ? "#ffffff" : "#000000"}"
-                        fill="${
-                          sortType === "default" || sortType === "asc"
-                            ? $isDarkMode
-                              ? "#ffffff"
-                              : "#000000"
-                            : ""
-                        }"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                      />
-                      <path
-                        d="M7.9313 9.00005H16.0686C16.6744 9.00005 16.9773 9.00005 17.1175 8.88025C17.2393 8.7763 17.3038 8.62038 17.2913 8.46082C17.2768 8.27693 17.0626 8.06274 16.6342 7.63436L12.5656 3.56573C12.3676 3.36772 12.2686 3.26872 12.1544 3.23163C12.054 3.199 11.9458 3.199 11.8454 3.23163C11.7313 3.26872 11.6323 3.36772 11.4342 3.56573L7.36561 7.63436C6.93724 8.06273 6.72305 8.27693 6.70858 8.46082C6.69602 8.62038 6.76061 8.7763 6.88231 8.88025C7.02257 9.00005 7.32548 9.00005 7.9313 9.00005Z"
-                         stroke="${$isDarkMode ? "#ffffff" : "#000000"}"
-                        fill="${
-                          sortType === "default" || sortType === "desc"
-                            ? $isDarkMode
-                              ? "#ffffff"
-                              : "#000000"
-                            : ""
-                        }"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                      />
-                    </g>
-                  </svg>`;
-  };
-
   const getPublicPortfolio = async () => {
     try {
       isLoading = true;
@@ -172,6 +127,15 @@
       default:
         sortNetWorth = "default";
     }
+
+    sortSharpeRatio = "default";
+    sortChange1D = "default";
+    sortChange7D = "default";
+    sortChange30D = "default";
+    sortChange1Y = "default";
+    sortMaxDrawDown = "default";
+    sortVolatility = "default";
+
     getPublicPortfolio();
   };
 
@@ -189,6 +153,15 @@
       default:
         sortChange1D = "default";
     }
+
+    sortNetWorth = "default";
+    sortSharpeRatio = "default";
+    sortChange7D = "default";
+    sortChange30D = "default";
+    sortChange1Y = "default";
+    sortMaxDrawDown = "default";
+    sortVolatility = "default";
+
     getPublicPortfolio();
   };
 
@@ -206,6 +179,15 @@
       default:
         sortChange7D = "default";
     }
+
+    sortNetWorth = "default";
+    sortSharpeRatio = "default";
+    sortChange1D = "default";
+    sortChange30D = "default";
+    sortChange1Y = "default";
+    sortMaxDrawDown = "default";
+    sortVolatility = "default";
+
     getPublicPortfolio();
   };
 
@@ -223,6 +205,15 @@
       default:
         sortChange30D = "default";
     }
+
+    sortNetWorth = "default";
+    sortSharpeRatio = "default";
+    sortChange1D = "default";
+    sortChange7D = "default";
+    sortChange1Y = "default";
+    sortMaxDrawDown = "default";
+    sortVolatility = "default";
+
     getPublicPortfolio();
   };
 
@@ -240,6 +231,15 @@
       default:
         sortChange1Y = "default";
     }
+
+    sortNetWorth = "default";
+    sortSharpeRatio = "default";
+    sortChange1D = "default";
+    sortChange7D = "default";
+    sortChange30D = "default";
+    sortMaxDrawDown = "default";
+    sortVolatility = "default";
+
     getPublicPortfolio();
   };
 
@@ -257,6 +257,15 @@
       default:
         sortMaxDrawDown = "default";
     }
+
+    sortNetWorth = "default";
+    sortSharpeRatio = "default";
+    sortChange1D = "default";
+    sortChange7D = "default";
+    sortChange30D = "default";
+    sortChange1Y = "default";
+    sortVolatility = "default";
+
     getPublicPortfolio();
   };
 
@@ -274,6 +283,15 @@
       default:
         sortSharpeRatio = "default";
     }
+
+    sortNetWorth = "default";
+    sortChange1D = "default";
+    sortChange7D = "default";
+    sortChange30D = "default";
+    sortChange1Y = "default";
+    sortMaxDrawDown = "default";
+    sortVolatility = "default";
+
     getPublicPortfolio();
   };
 
@@ -291,7 +309,61 @@
       default:
         sortVolatility = "default";
     }
+
+    sortNetWorth = "default";
+    sortSharpeRatio = "default";
+    sortChange1D = "default";
+    sortChange7D = "default";
+    sortChange30D = "default";
+    sortChange1Y = "default";
+    sortMaxDrawDown = "default";
+
     getPublicPortfolio();
+  };
+
+  $: sortIcon = (sortType) => {
+    return `<svg
+                    height="0.9rem"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <g id="SVGRepo_bgCarrier" stroke-width="0" /><g
+                      id="SVGRepo_tracerCarrier"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                    <g id="SVGRepo_iconCarrier">
+                      <path
+                        d="M16.0686 15H7.9313C7.32548 15 7.02257 15 6.88231 15.1198C6.76061 15.2238 6.69602 15.3797 6.70858 15.5393C6.72305 15.7232 6.93724 15.9374 7.36561 16.3657L11.4342 20.4344C11.6323 20.6324 11.7313 20.7314 11.8454 20.7685C11.9458 20.8011 12.054 20.8011 12.1544 20.7685C12.2686 20.7314 12.3676 20.6324 12.5656 20.4344L16.6342 16.3657C17.0626 15.9374 17.2768 15.7232 17.2913 15.5393C17.3038 15.3797 17.2392 15.2238 17.1175 15.1198C16.9773 15 16.6744 15 16.0686 15Z"
+                        stroke="${$isDarkMode ? "#ffffff" : "#000000"}"
+                        fill="${
+                          sortType === "default" || sortType === "desc"
+                            ? $isDarkMode
+                              ? "#ffffff"
+                              : "#000000"
+                            : ""
+                        }"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      />
+                      <path
+                        d="M7.9313 9.00005H16.0686C16.6744 9.00005 16.9773 9.00005 17.1175 8.88025C17.2393 8.7763 17.3038 8.62038 17.2913 8.46082C17.2768 8.27693 17.0626 8.06274 16.6342 7.63436L12.5656 3.56573C12.3676 3.36772 12.2686 3.26872 12.1544 3.23163C12.054 3.199 11.9458 3.199 11.8454 3.23163C11.7313 3.26872 11.6323 3.36772 11.4342 3.56573L7.36561 7.63436C6.93724 8.06273 6.72305 8.27693 6.70858 8.46082C6.69602 8.62038 6.76061 8.7763 6.88231 8.88025C7.02257 9.00005 7.32548 9.00005 7.9313 9.00005Z"
+                         stroke="${$isDarkMode ? "#ffffff" : "#000000"}"
+                        fill="${
+                          sortType === "default" || sortType === "asc"
+                            ? $isDarkMode
+                              ? "#ffffff"
+                              : "#000000"
+                            : ""
+                        }"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      />
+                    </g>
+                  </svg>`;
   };
 </script>
 
@@ -362,17 +434,7 @@
               >
                 Net Worth
                 <div on:click={toggleSortNetWorth} class="cursor-pointer">
-                  <!-- <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    height="1.2em"
-                    viewBox="0 0 320 512"
-                    fill={$isDarkMode ? "#fff" : "#000"}
-                  >
-                    <path
-                      d="M41 288h238c21.4 0 32.1 25.9 17 41L177 448c-9.4 9.4-24.6 9.4-33.9 0L24 329c-15.1-15.1-4.4-41 17-41zm255-105L177 64c-9.4-9.4-24.6-9.4-33.9 0L24 183c-15.1 15.1-4.4 41 17 41h238c21.4 0 32.1-25.9 17-41z"
-                    />
-                  </svg> -->
-                  {@html iconSorting(sortNetWorth)}
+                  {@html sortIcon(sortNetWorth)}
                 </div>
               </div>
             </th>
@@ -382,7 +444,7 @@
               >
                 1D
                 <div on:click={toggleSortChange1D} class="cursor-pointer">
-                  {@html iconSorting(sortChange1D)}
+                  {@html sortIcon(sortChange1D)}
                 </div>
               </div>
             </th>
@@ -392,7 +454,7 @@
               >
                 7D
                 <div on:click={toggleSortChange7D} class="cursor-pointer">
-                  {@html iconSorting(sortChange7D)}
+                  {@html sortIcon(sortChange7D)}
                 </div>
               </div>
             </th>
@@ -402,7 +464,7 @@
               >
                 30D
                 <div on:click={toggleSortChange30D} class="cursor-pointer">
-                  {@html iconSorting(sortChange30D)}
+                  {@html sortIcon(sortChange30D)}
                 </div>
               </div>
             </th>
@@ -427,7 +489,7 @@
                   Volatility
                 </TooltipTitle>
                 <div on:click={toggleSortVolatility} class="cursor-pointer">
-                  {@html iconSorting(sortVolatility)}
+                  {@html sortIcon(sortVolatility)}
                 </div>
               </div>
             </th>
@@ -442,7 +504,7 @@
                   Max drawdown
                 </TooltipTitle>
                 <div on:click={toggleSortMaxDrawDown} class="cursor-pointer">
-                  {@html iconSorting(sortMaxDrawDown)}
+                  {@html sortIcon(sortMaxDrawDown)}
                 </div>
               </div>
             </th>
@@ -457,7 +519,7 @@
                   Sharpe ratio
                 </TooltipTitle>
                 <div on:click={toggleSortSharpeRatio} class="cursor-pointer">
-                  {@html iconSorting(sortSharpeRatio)}
+                  {@html sortIcon(sortSharpeRatio)}
                 </div>
               </div>
             </th>
