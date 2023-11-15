06342e62e19c323f21cb542d24d481e28ee03626
huong666 <nguyenxuananhuong541@gmail.com>
2023-11-15 23:33:28 +0700

    delete console.log

--- src/layouts/WhalesList.svelte
+++ src/layouts/WhalesList.svelte
@@ -46,8 +46,6 @@
   let sortMaxDrawDown = "default";
   let sortVolatility = "default";
 
-  $: console.log("this is bla bla : ", sortNetWorth);
-
   $: iconSorting = () => {
     return `  <svg
                     height="0.9rem"
@@ -374,7 +372,7 @@
                       d="M41 288h238c21.4 0 32.1 25.9 17 41L177 448c-9.4 9.4-24.6 9.4-33.9 0L24 329c-15.1-15.1-4.4-41 17-41zm255-105L177 64c-9.4-9.4-24.6-9.4-33.9 0L24 183c-15.1 15.1-4.4 41 17 41h238c21.4 0 32.1-25.9 17-41z"
                     />
                   </svg> -->
-                  {@html iconSorting(sortVolatility)}
+                  {@html iconSorting()}
                 </div>
               </div>
             </th>
