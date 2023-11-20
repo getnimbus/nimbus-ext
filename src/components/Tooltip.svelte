bbead05a798f721e6728d6159b3dcdadcc0b789c
Thanh Le <mihthanh27@gmail.com>
2023-11-20 18:25:48 +0700

    Fix render issue with custom component

---
+++ src/components/Tooltip.svelte
@@ -0,0 +1,29 @@
+<script>
+  import { isDarkMode } from "~/store";
+
+  export let text = "";
+  export let link = "";
+</script>
+
+<div
+  class={`text-white font-normal text-left normal-case text-xs py-1 px-2 rounded-lg w-max xl:max-w-md max-w-screen-sm border-[0.5px] border-[#cdcdcd59] ${
+    $isDarkMode ? "bg-[#0f0f0f]" : "bg-black"
+  }`}
+  style="box-shadow: 0px 4px 20px rgba(0, 0, 0, 0.15);"
+>
+  {#if link.length !== 0}
+    {text}
+    <a
+      href={link}
+      target="_blank"
+      class="hover:text-blue-500 cursor-pointer text-white"
+    >
+      Learn more
+    </a>
+  {:else}
+    {text}
+  {/if}
+</div>
+
+<style>
+</style>
