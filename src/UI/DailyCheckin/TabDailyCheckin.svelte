567f8a920cddb5e176615e0938b332f393d23973
binhnguyen2501 <binhq6@gmail.com>
2023-12-07 16:31:29 +0700

    update flow bonus twitter

--- src/UI/DailyCheckin/TabDailyCheckin.svelte
+++ src/UI/DailyCheckin/TabDailyCheckin.svelte
@@ -6,6 +6,8 @@
   import { dailyCheckinTypePortfolio, triggerFirework } from "~/utils";
   import dayjs from "dayjs";
   import { wait } from "~/entries/background/utils";
+  import { Toast } from "flowbite-svelte";
+  import { blur } from "svelte/transition";
 
   import Button from "~/components/Button.svelte";
   import Loading from "~/components/Loading.svelte";
@@ -39,6 +41,24 @@
   let dataCheckinHistory = [];
   let sortTypeHistory = "default";
   let sortPointHistory = "default";
+
+  let toastMsg = "";
+  let isSuccessToast = false;
+  let counter = 3;
+  let showToast = false;
+
+  const trigger = () => {
+    showToast = true;
+    counter = 3;
+    timeout();
+  };
+
+  const timeout = () => {
+    if (--counter > 0) return setTimeout(timeout, 1000);
+    showToast = false;
+    toastMsg = "";
+    isSuccessToast = false;
+  };
 
   const queryClient = useQueryClient();
 
@@ -308,6 +328,31 @@
                     </g>
                   </svg>`;
   };
+
+  const handleReceiveQuest = async (link: string, type: string) => {
+    try {
+      if (type === "first-share-on-twitter") {
+        window.open(link, "_blank");
+        await wait(5000);
+        const res = await nimbus.get(
+          `/v2/checkin/${$userPublicAddress}/twitter-quest`
+        );
+        if (res && res?.data === null) {
+          toastMsg = "You already post us on Twitter";
+          isSuccessToast = false;
+          trigger();
+        }
+        if (res?.data?.bonus !== undefined) {
+          triggerCheckinSuccess();
+          bonusScore = res?.data?.bonus;
+          isTriggerBonusScore = true;
+          queryClient.invalidateQueries([$userPublicAddress, "daily-checkin"]);
+        }
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  };
 </script>
 
 <div class="flex flex-col gap-4 min-h-screen">
@@ -524,11 +569,20 @@
                         </div>
                       </div>
                       <div class="w-[170px]">
-                        <a href={quest?.url}>
-                          <Button>
-                            <div class="py-1">Collect!</div>
-                          </Button>
-                        </a>
+                        {#if quest?.isInternalLink}
+                          <a href={quest?.url} class="py-1">
+                            <Button>Collect!</Button>
+                          </a>
+                        {:else}
+                          <div
+                            on:click={() => {
+                              handleReceiveQuest(quest?.url, quest?.id);
+                            }}
+                            class="py-1"
+                          >
+                            <Button>Collect!</Button>
+                          </div>
+                        {/if}
                       </div>
                     </div>
                   {/each}
@@ -669,6 +723,51 @@
         You have received {bonusScore} Bonus GM Points
       </div>
     </div>
+  </div>
+{/if}
+
+{#if showToast}
+  <div class="fixed top-3 right-3 w-full z-10">
+    <Toast
+      transition={blur}
+      params={{ amount: 10 }}
+      position="top-right"
+      color={isSuccessToast ? "green" : "red"}
+      bind:open={showToast}
+    >
+      <svelte:fragment slot="icon">
+        {#if isSuccessToast}
+          <svg
+            aria-hidden="true"
+            class="w-5 h-5"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+            ><path
+              fill-rule="evenodd"
+              d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+              clip-rule="evenodd"
+            /></svg
+          >
+          <span class="sr-only">Check icon</span>
+        {:else}
+          <svg
+            aria-hidden="true"
+            class="w-5 h-5"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+            ><path
+              fill-rule="evenodd"
+              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+              clip-rule="evenodd"
+            /></svg
+          >
+          <span class="sr-only">Error icon</span>
+        {/if}
+      </svelte:fragment>
+      {toastMsg}
+    </Toast>
   </div>
 {/if}
 
