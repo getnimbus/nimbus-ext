d18b657bd7ac6e6ae8eed1fa8bc78cbfae111c50
binhnguyen2501 <binhq6@gmail.com>
2023-12-09 19:33:40 +0700

    update testimonial

---
+++ src/UI/Testimonial/SlideFour.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import Avatar from "~/assets/Anh_Tran.jpeg";
+</script>
+
+<div class="flex flex-col gap-5 h-[35vh]">
+  <blockquote class="text-lg font-normal leading-8 tracking-tight">
+    <p>
+      Go checkout Nimbus platform, really great approach on UI/UX stuff, team is
+      putting a lot of effort releasing features/updates quickly. Worth a try.
+    </p>
+  </blockquote>
+  <figcaption class="flex items-center gap-x-6">
+    <img
+      class="h-12 w-12 rounded-full bg-gray-50"
+      src={Avatar}
+      alt="Thanh Le"
+    />
+    <div class="text-sm">
+      <div class="font-semibold">Geninsus.sol | SonarWatch</div>
+      <div class="text-gray-500">
+        Co-Founder
+        <a
+          href="https://twitter.com/Sonarwatch"
+          target="_blank"
+          class="text-blue-500">@Sonarwatch</a
+        >
+      </div>
+    </div>
+  </figcaption>
+</div>
+
+<style></style>
