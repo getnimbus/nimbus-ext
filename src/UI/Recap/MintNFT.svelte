d9d0de9e6963e74aeb582ee1b886583bf4632bc4
Thanh Le <mihthanh27@gmail.com>
2023-12-29 10:39:11 +0700

    Example to mint img

--- src/UI/Recap/MintNFT.svelte
+++ src/UI/Recap/MintNFT.svelte
@@ -1,5 +1,43 @@
-<script></script>
+<script>
+  import {
+    createQuery,
+    createMutation,
+    useQueryClient,
+  } from "@tanstack/svelte-query";
+  import { nimbus } from "~/lib/network";
+  import { walletStore } from "@svelte-on-solana/wallet-adapter-core";
+  import {
+    Connection,
+    Transaction,
+    VersionedTransaction,
+  } from "@solana/web3.js";
 
-<div class="h-screen">MintnFT</div>
+  const handleMintNFT = createMutation({
+    mutationFn: async () => {
+      const data = await nimbus.post("/recap/mint-nft", {
+        image: "", // TODO: Add base 64 image here
+      });
+
+      console.log(data.data.result.encoded_transaction);
+      const signedTx = await $walletStore.signTransaction(
+        Transaction.from(
+          Buffer.from(data.data.result.encoded_transaction, "base64")
+        )
+      );
+      const connection = new Connection(
+        "https://rpc.shyft.to?api_key=Qjb6SubTTbLrkmNo"
+      );
+      const result = await $walletStore.sendTransaction(signedTx, connection);
+      console.log(result);
+      return result;
+    },
+  });
+</script>
+
+<div class="h-screen">
+  MintnFT
+
+  <button on:click={$handleMintNFT.mutate()}>Mint NFT</button>
+</div>
 
 <style></style>
