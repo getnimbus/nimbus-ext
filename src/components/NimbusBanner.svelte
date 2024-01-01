93fb05643c861add4212c5f05c9522390328c842
huong666 <nguyenxuananhuong541@gmail.com>
2024-01-01 22:34:09 +0700

    fix recap banner content

--- src/components/NimbusBanner.svelte
+++ src/components/NimbusBanner.svelte
@@ -24,41 +24,23 @@
     </div>
     <div class="flex flex-wrap items-center gap-x-4 gap-y-2">
       <p class="text-sm leading-6 text-[#27326f]">
-        <strong class="font-semibold">Nimbus Recap 2023</strong>
+        <strong class="font-semibold">Solana Recap 2023</strong>
         <svg
           viewBox="0 0 2 2"
           class="mx-2 inline h-0.5 w-0.5 fill-current"
           aria-hidden="true"
         >
           <circle cx="1" cy="1" r="1" />
-        </svg>Discover the unforgettable moments of 2023 – a year of milestones
-        and achievements
+        </svg>Discover the unforgettable moments of 2023 with Solana – a year of
+        remarkable moments
       </p>
       <Link to="recap">
         <div
           class="flex-none rounded-full bg-[#27326f] px-3.5 py-1 text-sm font-semibold text-white shadow-sm"
         >
-          See your recap <span aria-hidden="true">&rarr;</span>
+          View your recap <span aria-hidden="true">&rarr;</span>
         </div>
       </Link>
-    </div>
-    <div class="flex flex-1 justify-end">
-      <button
-        type="button"
-        class="-m-3 p-3 focus-visible:outline-offset-[-4px]"
-      >
-        <span class="sr-only">Dismiss</span>
-        <svg
-          class="h-5 w-5 text-[#27326f]"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-          aria-hidden="true"
-        >
-          <path
-            d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z"
-          />
-        </svg>
-      </button>
     </div>
   </div>
 </div>
