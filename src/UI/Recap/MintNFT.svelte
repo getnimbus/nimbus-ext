6cba3241935949429f5c9985420bc4639aeb41a2
Thanh Le <mihthanh27@gmail.com>
2024-01-02 17:53:55 +0700

    Add sharing in mint button

--- src/UI/Recap/MintNFT.svelte
+++ src/UI/Recap/MintNFT.svelte
@@ -390,7 +390,7 @@
           Tweet your top moments, achievements, and favorite memories using our
           Nimbus Recap NFT
         </div>
-        <div on:click={() => console.log("sharing tweet")}>
+        <a href="https://twitter.com/intent/tweet?text=2023%20has%20proven%20to%20be%20a%20challenging%20year%20for%20every%20holder%20and%20now%20let%27s%20reflect%20on%20my%20journey%20with%20%40solana%0Ahttps%3A%2F%2Fapp.getnimbus.io%2Frecap%0AIt%27s%20cool%20to%20be%20a%20part%20of%20Solana%0A%0A%23SolanaRE2023">
           <Button>
             <div class="py-2 px-3">Share!</div>
           </Button>
