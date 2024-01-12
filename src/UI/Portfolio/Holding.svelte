ebf6553f8de4fd0b1c1adce181cacd00802fbd55
binhnguyen2501 <binhq6@gmail.com>
2024-01-12 17:11:23 +0700

    filter symbol and contract address

--- src/UI/Portfolio/Holding.svelte
+++ src/UI/Portfolio/Holding.svelte
@@ -116,31 +116,35 @@
           $typeWallet === "CEX" &&
           filteredUndefinedCmcHoldingTokenData.length > 0
         ) {
-          filteredUndefinedCmcHoldingTokenData.map((item) => {
-            priceMobulaSubscribe([item?.symbol], "CEX", (data) => {
-              marketPriceToken = {
-                id: data.id,
-                market_price: data.price,
-              };
+          filteredUndefinedCmcHoldingTokenData
+            .filter((item) => item?.symbol)
+            .map((item) => {
+              priceMobulaSubscribe([item?.symbol], "CEX", (data) => {
+                marketPriceToken = {
+                  id: data.id,
+                  market_price: data.price,
+                };
+              });
             });
-          });
         }
 
         const chainList = Object.keys(groupFilteredNullCmcHoldingTokenData);
 
         chainList.map((chain) => {
-          groupFilteredNullCmcHoldingTokenData[chain].map((item) => {
-            priceMobulaSubscribe(
-              [item?.contractAddress],
-              item?.chain,
-              (data) => {
-                marketPriceToken = {
-                  id: data.id,
-                  market_price: data.price,
-                };
-              }
-            );
-          });
+          groupFilteredNullCmcHoldingTokenData[chain]
+            .filter((item) => item?.contractAddress)
+            .map((item) => {
+              priceMobulaSubscribe(
+                [item?.contractAddress],
+                item?.chain,
+                (data) => {
+                  marketPriceToken = {
+                    id: data.id,
+                    market_price: data.price,
+                  };
+                }
+              );
+            });
         });
 
         dataSubWS = filteredHoldingTokenData.map((item) => {
