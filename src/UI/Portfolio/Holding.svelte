61f1ec808e5faafbc8e503433a3a1c9a7004471f
binhnguyen2501 <binhq6@gmail.com>
2024-01-15 19:34:15 +0700

    update condition

--- src/UI/Portfolio/Holding.svelte
+++ src/UI/Portfolio/Holding.svelte
@@ -175,19 +175,12 @@
             chunk
               .filter((item) => item?.symbol)
               .map((item) => {
-                priceMobulaSubscribe(
-                  {
-                    symbol: item?.symbol,
-                    contract_address: item?.contractAddress,
-                  },
-                  "CEX",
-                  (data) => {
-                    marketPriceToken = {
-                      id: data.id,
-                      market_price: data.price,
-                    };
-                  }
-                );
+                priceMobulaSubscribe([item?.symbol], "CEX", (data) => {
+                  marketPriceToken = {
+                    id: data.id,
+                    market_price: data.price,
+                  };
+                });
               });
           });
         }
@@ -203,10 +196,7 @@
               .filter((item) => item?.contractAddress)
               .map((item) => {
                 priceMobulaSubscribe(
-                  {
-                    symbol: item?.symbol,
-                    contract_address: item?.contractAddress,
-                  },
+                  [item?.contractAddress],
                   item?.chain,
                   (data) => {
                     marketPriceToken = {
