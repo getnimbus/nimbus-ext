81c06ca941c36d7f5bff30ea610daa1d6864c063
Thanh Le <mihthanh27@gmail.com>
2024-02-24 18:45:24 +0700

    Fix calling to query vault

--- src/layouts/Portfolio.svelte
+++ src/layouts/Portfolio.svelte
@@ -440,6 +440,7 @@
 
   //// VAULTS
   const getVaults = async (address, chain) => {
+    return []; // TODO: Fix the api query time later
     let addressChain = chain;
 
     if (addressChain === "ALL") {
