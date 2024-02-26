0eb669b9b69392a1abf7addc13d81289a88cbdf5
binhnguyen2501 <binhq6@gmail.com>
2024-02-26 10:19:25 +0700

    update toast msg login

--- src/UI/Auth/SUIAuth.svelte
+++ src/UI/Auth/SUIAuth.svelte
@@ -5,6 +5,8 @@
   import { SuiConnector, WalletState } from "nimbus-sui-kit";
   import { isDarkMode, user, suiWalletInstance } from "~/store";
   import { nimbus } from "~/lib/network";
+  import { Toast } from "flowbite-svelte";
+  import { blur } from "svelte/transition";
 
   import ReactAdapter from "~/components/ReactAdapter.svelte";
 
@@ -45,6 +47,24 @@
   };
 
   let invitation = "";
+
+  let toastMsg = "";
+  let isSuccessToast = false;
+  let counter = 3;
+  let showToast = false;
+
+  const trigger = () => {
+    showToast = true;
+    counter = 3;
+    timeout();
+  };
+
+  const timeout = () => {
+    if (--counter > 0) return setTimeout(timeout, 1000);
+    showToast = false;
+    toastMsg = "";
+    isSuccessToast = false;
+  };
 
   onMount(() => {
     const urlParams = new URLSearchParams(window.location.search);
@@ -120,9 +140,16 @@
               picture: User,
             })
         );
+        toastMsg = "Login with Sui successfully!";
+        isSuccessToast = false;
+        trigger();
         queryClient.invalidateQueries(["users-me"]);
         queryClient.invalidateQueries(["list-address"]);
       }
+
+      toastMsg = res?.error;
+      isSuccessToast = false;
+      trigger();
     } catch (e) {
       console.error("error: ", e);
     }
@@ -146,6 +173,51 @@
   {chains}
   integrator="svelte-example"
 />
+
+{#if showToast}
+  <div class="fixed top-3 right-3 w-full" style="z-index: 2147483648;">
+    <Toast
+      transition={blur}
+      params={{ amount: 10 }}
+      position="top-right"
+      color={isSuccessToast ? "green" : "red"}
+      bind:open={showToast}
+    >
+      <svelte:fragment slot="icon">
+        {#if isSuccessToast}
+          <svg
+            aria-hidden="true"
+            class="w-5 h-5"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+            ><path
+              fill-rule="evenodd"
+              d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+              clip-rule="evenodd"
+            /></svg
+          >
+          <span class="sr-only">Check icon</span>
+        {:else}
+          <svg
+            aria-hidden="true"
+            class="w-5 h-5"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+            ><path
+              fill-rule="evenodd"
+              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+              clip-rule="evenodd"
+            /></svg
+          >
+          <span class="sr-only">Error icon</span>
+        {/if}
+      </svelte:fragment>
+      {toastMsg}
+    </Toast>
+  </div>
+{/if}
 
 <style>
 </style>
