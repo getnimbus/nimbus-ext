228fa591880dfd181d4d8548c722023c2f88eb48
binhnguyen2501 <binhq6@gmail.com>
2024-03-26 21:54:50 +0700

    update table borrow

--- src/UI/Portfolio/DefiPosition/BorrowPosition.svelte
+++ src/UI/Portfolio/DefiPosition/BorrowPosition.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
   import { isDarkMode } from "~/store";
-  import { flatten } from "lodash";
 
   import Image from "~/components/Image.svelte";
   import TooltipNumber from "~/components/TooltipNumber.svelte";
@@ -38,18 +37,174 @@
 </script>
 
 {#if dataBorrowLendingStaking && dataBorrowLendingStaking?.data.length !== 0}
-  <LendingPosition
-    data={flatten(
-      dataBorrowLendingStaking.data.map((item) => {
-        return item.input.map((eachInput) => eachInput);
-      })
-    )}
-  />
+  <div class="flex flex-col gap-4">
+    {#each dataBorrowLendingStaking?.data as itemRow}
+      <div
+        class={`rounded-[10px] overflow-hidden w-full ${
+          $isDarkMode ? "bg-[#131313]" : "bg-[#fff] border border_0000000d"
+        }`}
+      >
+        <div class="bg_f4f5f8 grid grid-cols-4">
+          <div class="col-spans-2 pl-3 py-3 rounded-tl-[10px]">
+            <div class="text-left text-xs uppercase font-medium">Supplied</div>
+          </div>
+
+          <div class="py-3">
+            <div class="text-xs uppercase font-medium">Balance</div>
+          </div>
+
+          <div class="py-3 pr-3 rounded-tr-[10px]">
+            <div class="text-right text-xs uppercase font-medium">
+              USD Value
+            </div>
+          </div>
+        </div>
+
+        {#each itemRow?.current?.tokens || [] as item}
+          <div class="grid grid-cols-4 group transition-all">
+            <div
+              class={`col-spans-2 pl-3 py-3 ${
+                $isDarkMode
+                  ? "bg-[#131313] group-hover:bg-[#000]"
+                  : "bg-white group-hover:bg-gray-100"
+              }`}
+            >
+              <div class="flex flex-col gap-2 m-auto h-full">
+                <div class="flex items-center gap-3">
+                  <div class="rounded-full w-6 h-6 overflow-hidden">
+                    <Image
+                      defaultLogo={defaultToken}
+                      logo={item?.token?.logo || defaultToken}
+                    />
+                  </div>
+                  <div class="text-left text-sm font-medium">
+                    {item?.token?.name || ""}
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div
+              class={`py-3 ${
+                $isDarkMode
+                  ? "bg-[#131313] group-hover:bg-[#000]"
+                  : "bg-white group-hover:bg-gray-100"
+              }`}
+            >
+              <div
+                class="text-left text-sm text_00000099 font-medium gap-2 flex items-center gap-1"
+              >
+                <TooltipNumber number={item?.amount || 0} type="amount" />
+                {item?.token?.chain}
+              </div>
+            </div>
+
+            <div
+              class={`py-3 pr-3 ${
+                $isDarkMode
+                  ? "bg-[#131313] group-hover:bg-[#000]"
+                  : "bg-white group-hover:bg-gray-100"
+              }`}
+            >
+              <div
+                class="text-right text-sm text_00000099 font-medium flex justify-end h-full"
+              >
+                <div class="my-auto">
+                  <TooltipNumber number={item?.value || 0} type="value" />
+                </div>
+              </div>
+            </div>
+          </div>
+        {/each}
+      </div>
+
+      <LendingPosition data={itemRow?.input} />
+    {/each}
+  </div>
 {/if}
 
 {#if dataBorrow && dataBorrow?.data.length !== 0}
-  <div class="flex flex-col gap-2">
+  <div class="flex flex-col gap-4">
     {#each dataBorrow?.data as itemRow}
+      <div
+        class={`rounded-[10px] overflow-hidden w-full ${
+          $isDarkMode ? "bg-[#131313]" : "bg-[#fff] border border_0000000d"
+        }`}
+      >
+        <div class="bg_f4f5f8 grid grid-cols-4">
+          <div class="col-spans-2 pl-3 py-3 rounded-tl-[10px]">
+            <div class="text-left text-xs uppercase font-medium">Supplied</div>
+          </div>
+
+          <div class="py-3">
+            <div class="text-xs uppercase font-medium">Balance</div>
+          </div>
+
+          <div class="py-3 pr-3 rounded-tr-[10px]">
+            <div class="text-right text-xs uppercase font-medium">
+              USD Value
+            </div>
+          </div>
+        </div>
+
+        {#each itemRow?.input || [] as item}
+          <div class="grid grid-cols-4 group transition-all">
+            <div
+              class={`col-spans-2 pl-3 py-3 ${
+                $isDarkMode
+                  ? "bg-[#131313] group-hover:bg-[#000]"
+                  : "bg-white group-hover:bg-gray-100"
+              }`}
+            >
+              <div class="flex flex-col gap-2 m-auto h-full">
+                <div class="flex items-center gap-3">
+                  <div class="rounded-full w-6 h-6 overflow-hidden">
+                    <Image
+                      defaultLogo={defaultToken}
+                      logo={item?.token?.logo || defaultToken}
+                    />
+                  </div>
+                  <div class="text-left text-sm font-medium">
+                    {item?.token?.name || ""}
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div
+              class={`py-3 ${
+                $isDarkMode
+                  ? "bg-[#131313] group-hover:bg-[#000]"
+                  : "bg-white group-hover:bg-gray-100"
+              }`}
+            >
+              <div
+                class="text-left text-sm text_00000099 font-medium gap-2 flex items-center gap-1"
+              >
+                <TooltipNumber number={item?.amount || 0} type="amount" />
+                {item?.token?.chain}
+              </div>
+            </div>
+
+            <div
+              class={`py-3 pr-3 ${
+                $isDarkMode
+                  ? "bg-[#131313] group-hover:bg-[#000]"
+                  : "bg-white group-hover:bg-gray-100"
+              }`}
+            >
+              <div
+                class="text-right text-sm text_00000099 font-medium flex justify-end h-full"
+              >
+                <div class="my-auto">
+                  <TooltipNumber number={item?.value || 0} type="value" />
+                </div>
+              </div>
+            </div>
+          </div>
+        {/each}
+      </div>
+
       <div
         class={`rounded-[10px] overflow-hidden w-full ${
           $isDarkMode ? "bg-[#131313]" : "bg-[#fff] border border_0000000d"
@@ -209,85 +364,6 @@
             </div>
           </div>
         </div>
-      </div>
-
-      <div
-        class={`rounded-[10px] overflow-hidden w-full ${
-          $isDarkMode ? "bg-[#131313]" : "bg-[#fff] border border_0000000d"
-        }`}
-      >
-        <div class="bg_f4f5f8 grid grid-cols-4">
-          <div class="col-spans-2 pl-3 py-3 rounded-tl-[10px]">
-            <div class="text-left text-xs uppercase font-medium">Supplied</div>
-          </div>
-
-          <div class="py-3">
-            <div class="text-xs uppercase font-medium">Balance</div>
-          </div>
-
-          <div class="py-3 pr-3 rounded-tr-[10px]">
-            <div class="text-right text-xs uppercase font-medium">
-              USD Value
-            </div>
-          </div>
-        </div>
-
-        {#each itemRow?.input || [] as item}
-          <div class="grid grid-cols-4 group transition-all">
-            <div
-              class={`col-spans-2 pl-3 py-3 ${
-                $isDarkMode
-                  ? "bg-[#131313] group-hover:bg-[#000]"
-                  : "bg-white group-hover:bg-gray-100"
-              }`}
-            >
-              <div class="flex flex-col gap-2 m-auto h-full">
-                <div class="flex items-center gap-3">
-                  <div class="rounded-full w-6 h-6 overflow-hidden">
-                    <Image
-                      defaultLogo={defaultToken}
-                      logo={item?.token?.logo || defaultToken}
-                    />
-                  </div>
-                  <div class="text-left text-sm font-medium">
-                    {item?.token?.name || ""}
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div
-              class={`py-3 ${
-                $isDarkMode
-                  ? "bg-[#131313] group-hover:bg-[#000]"
-                  : "bg-white group-hover:bg-gray-100"
-              }`}
-            >
-              <div
-                class="text-left text-sm text_00000099 font-medium gap-2 flex items-center gap-1"
-              >
-                <TooltipNumber number={item?.amount || 0} type="amount" />
-                {item?.token?.chain}
-              </div>
-            </div>
-
-            <div
-              class={`py-3 pr-3 ${
-                $isDarkMode
-                  ? "bg-[#131313] group-hover:bg-[#000]"
-                  : "bg-white group-hover:bg-gray-100"
-              }`}
-            >
-              <div
-                class="text-right text-sm text_00000099 font-medium flex justify-end h-full"
-              >
-                <div class="my-auto">
-                  <TooltipNumber number={item?.value || 0} type="value" />
-                </div>
-              </div>
-            </div>
-          </div>
-        {/each}
       </div>
     {/each}
   </div>
