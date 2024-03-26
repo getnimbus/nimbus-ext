6de71255b55ee86279e4aa86b22d19214221f1c4
huong666 <nguyenxuananhuong541@gmail.com>
2024-03-26 15:09:57 +0700

    responsive point tracker

--- src/UI/Portfolio/PointsTracker.svelte
+++ src/UI/Portfolio/PointsTracker.svelte
@@ -42,8 +42,6 @@
     },
   };
 
-  let tableTokenHeader;
-  let isStickyTableToken = false;
   let clickOnX = false;
 
   const getPointsAirdrop = async () => {
@@ -55,17 +53,6 @@
     queryFn: () => getPointsAirdrop(),
     staleTime: Infinity,
     enabled: $user && Object.keys($user).length !== 0,
-  });
-
-  onMount(() => {
-    const handleScroll = () => {
-      const clientRectTokenHeader = tableTokenHeader?.getBoundingClientRect();
-      isStickyTableToken = clientRectTokenHeader?.top <= 0;
-    };
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
   });
 
   $: dataquery =
@@ -97,19 +84,14 @@
     points: $pointsQuery?.data?.totalPoint,
     protocol: "gmpoints",
     protocolLabel: "GMPoints",
-    price: 0,
+    price: null,
   };
-
-  $: console.log({
-    data: $pointsQuery.data,
-    dataWhalesMarket: $pointsQuery?.data?.dataWhalesMarket,
-    dataquery,
-  });
 </script>
 
 <ErrorBoundary>
+  <!-- Desktop -->
   <div
-    class={`xl:block hidden rounded-[10px] xl:overflow-visible overflow-x-auto h-full ${
+    class={`xl:block hidden rounded-[10px] overflow-visible h-full ${
       $isDarkMode ? "bg-[#131313]" : "bg-[#fff] border border_0000000d"
     }`}
   >
@@ -121,30 +103,19 @@
       </div>
     {:else}
       {#each dataquery || [] as data}
-        <table class="table-auto xl:w-full w-[2000px] h-full">
-          <thead
-            class={isStickyTableToken ? "sticky top-0 z-9" : ""}
-            bind:this={tableTokenHeader}
-          >
+        <table class="table-auto w-full h-full">
+          <thead>
             <tr class="bg_f4f5f8">
-              <th
-                class="pl-3 py-3 rounded-tl-[10px] xl:static xl:bg-transparent sticky left-0 z-10 bg_f4f5f8 w-[200px]"
-              >
-                <div class="text-left xl:text-xs text-xl uppercase font-medium">
-                  Name
+              <th class="pl-3 py-3 rounded-tl-[10px] bg-transparent bg_f4f5f8">
+                <div class="text-left text-xs uppercase font-medium">Name</div>
+              </th>
+              <th class="py-3 pr-3">
+                <div class="text-right text-xs uppercase font-medium">
+                  Points
                 </div>
               </th>
               <th class="py-3 pr-3 rounded-tr-[10px]">
-                <div
-                  class="text-right xl:text-xs text-xl uppercase font-medium"
-                >
-                  Point
-                </div>
-              </th>
-              <th class="py-3 pr-3 rounded-tr-[10px]">
-                <div
-                  class="text-right xl:text-xs text-xl uppercase font-medium"
-                >
+                <div class="text-right text-xs uppercase font-medium">
                   Estimated value
                 </div>
               </th>
@@ -152,24 +123,16 @@
           </thead>
           <tbody>
             {#each [gmPoint].concat(data.points) as item}
-              <tr
-                class="group transition-all cursor-pointer relative"
-                on:click={() =>
-                  !clickOnX &&
-                  window.open(
-                    protocolInfo[item?.protocolLabel].website,
-                    "_blank"
-                  )}
-              >
+              <tr class="group transition-all relative">
                 <td
-                  class={`pl-3 py-3 xl:static sticky left-0 z-9 w-[200px] ${
+                  class={`pl-3 py-3 ${
                     $isDarkMode
                       ? "bg-[#131313] group-hover:bg-[#000]"
                       : "bg-white group-hover:bg-gray-100"
                   }`}
                 >
                   <div
-                    class=" flex justify-start items-center gap-3 text-2xl font-medium xl:text-sm"
+                    class=" flex justify-start items-center gap-3 text-sm font-medium"
                   >
                     <div class="rounded-full w-[30px] h-[30px] overflow-hidden">
                       <Image
@@ -177,39 +140,82 @@
                         defaultLogo={defaultToken}
                       />
                     </div>
-                    <div>
+                    <div class="flex flex-col gap-1">
                       {protocolInfo[item?.protocolLabel].name}
-                    </div>
-
-                    {#if protocolInfo[item?.protocolLabel].twitter}
-                      <a
-                        href={protocolInfo[item?.protocolLabel].twitter}
-                        target="_blank"
-                        on:mouseenter={() => (clickOnX = true)}
-                        on:mouseleave={() => (clickOnX = false)}
-                      >
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="18"
-                          height="18"
-                          viewBox="0 0 14 14"
-                        >
-                          <rect width="14" height="14" fill="none" />
-                          <g fill="none">
-                            <g clip-path="url(#primeTwitter0)">
+                      <div class="flex items-center gap-1">
+                        {#if protocolInfo[item?.protocolLabel].twitter}
+                          <a
+                            href={protocolInfo[item?.protocolLabel].twitter}
+                            target="_blank"
+                            on:mouseenter={() => (clickOnX = true)}
+                            on:mouseleave={() => (clickOnX = false)}
+                          >
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              width="14"
+                              height="14"
+                              viewBox="0 0 14 14"
+                            >
+                              <rect width="14" height="14" fill="none" />
+                              <g fill="none">
+                                <g clip-path="url(#primeTwitter0)">
+                                  <path
+                                    fill="currentColor"
+                                    d="M11.025.656h2.147L8.482 6.03L14 13.344H9.68L6.294 8.909l-3.87 4.435H.275l5.016-5.75L0 .657h4.43L7.486 4.71zm-.755 11.4h1.19L3.78 1.877H2.504z"
+                                  />
+                                </g>
+                                <defs>
+                                  <clipPath id="primeTwitter0">
+                                    <path fill="#fff" d="M0 0h14v14H0z" />
+                                  </clipPath>
+                                </defs>
+                              </g>
+                            </svg>
+                          </a>
+                        {/if}
+                        {#if protocolInfo[item?.protocolLabel].website}
+                          <a
+                            href={protocolInfo[item?.protocolLabel].website}
+                            target="_blank"
+                            on:mouseenter={() => (clickOnX = true)}
+                            on:mouseleave={() => (clickOnX = false)}
+                          >
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              width="18"
+                              height="18"
+                              viewBox="0 0 256 256"
+                            >
+                              <rect width="14" height="14" fill="none" />
                               <path
                                 fill="currentColor"
-                                d="M11.025.656h2.147L8.482 6.03L14 13.344H9.68L6.294 8.909l-3.87 4.435H.275l5.016-5.75L0 .657h4.43L7.486 4.71zm-.755 11.4h1.19L3.78 1.877H2.504z"
+                                d="M137.54 186.36a8 8 0 0 1 0 11.31l-9.94 10a56 56 0 0 1-79.22-79.27l24.12-24.12a56 56 0 0 1 76.81-2.28a8 8 0 1 1-10.64 12a40 40 0 0 0-54.85 1.63L59.7 139.72a40 40 0 0 0 56.58 56.58l9.94-9.94a8 8 0 0 1 11.32 0m70.08-138a56.08 56.08 0 0 0-79.22 0l-9.94 9.95a8 8 0 0 0 11.32 11.31l9.94-9.94a40 40 0 0 1 56.58 56.58l-24.12 24.14a40 40 0 0 1-54.85 1.6a8 8 0 1 0-10.64 12a56 56 0 0 0 76.81-2.26l24.12-24.12a56.08 56.08 0 0 0 0-79.24Z"
                               />
-                            </g>
-                            <defs>
-                              <clipPath id="primeTwitter0">
-                                <path fill="#fff" d="M0 0h14v14H0z" />
-                              </clipPath>
-                            </defs>
-                          </g>
-                        </svg>
-                      </a>
+                            </svg>
+                          </a>
+                        {/if}
+                      </div>
+                    </div>
+                  </div>
+                </td>
+
+                <td
+                  class={`py-3 pr-3 ${
+                    $isDarkMode
+                      ? "group-hover:bg-[#000]"
+                      : "group-hover:bg-gray-100"
+                  }`}
+                >
+                  <div
+                    class="flex justify-end text-sm font-medium text_00000099"
+                  >
+                    {#if item.protocolLabel === "GMPoints"}
+                      {0}
+                    {:else}
+                      <TooltipNumber
+                        number={item?.points || 0}
+                        type="balance"
+                      />
                     {/if}
                   </div>
                 </td>
@@ -222,28 +228,14 @@
                   }`}
                 >
                   <div
-                    class="flex justify-end text-2xl font-medium xl:text-sm text_00000099"
-                  >
-                    <TooltipNumber number={item?.points || 0} type="balance" />
-                  </div>
-                </td>
-
-                <td
-                  class={`py-3 pr-3 ${
-                    $isDarkMode
-                      ? "group-hover:bg-[#000]"
-                      : "group-hover:bg-gray-100"
-                  }`}
-                >
-                  <div
-                    class="flex justify-end text-2xl font-medium xl:text-sm text_00000099"
+                    class="flex justify-end text-sm font-medium text_00000099"
                   >
                     {#if item?.price === null}
-                      Invalidated
+                      Not available
                     {:else}
                       <TooltipNumber
                         number={item?.price * item?.points || 0}
-                        type="balance"
+                        type="value"
                       />
                     {/if}
                   </div>
@@ -253,6 +245,103 @@
           </tbody>
         </table>
       {/each}
+    {/if}
+  </div>
+
+  <!-- Mobile  -->
+  <div
+    class={`xl:hidden rounded-[10px] overflow-visible h-full ${
+      $isDarkMode ? "bg-[#131313]" : "bg-[#fff] border border_0000000d"
+    }`}
+  >
+    {#if $pointsQuery.isLoading}
+      <div>
+        <div class="flex justify-center items-center h-full py-3 px-3">
+          <Loading />
+        </div>
+      </div>
+    {:else}
+      {#each dataquery || [] as data}
+        {#each [gmPoint].concat(data.points) || [] as item}
+          <div class="relative flex flex-col gap-2 px-2 py-4">
+            <div class="flex justify-between">
+              <div class="flex gap-2 items-center font-medium">
+                <div class="w-[30px] h-[30px] overflow-hidden rounded-full">
+                  <Image
+                    logo={protocolInfo[item?.protocolLabel].logo}
+                    defaultLogo={defaultToken}
+                  />
+                </div>
+                {protocolInfo[item?.protocolLabel].name}
+              </div>
+              <div class="flex items-center gap-1">
+                {#if protocolInfo[item?.protocolLabel].twitter}
+                  <a
+                    href={protocolInfo[item?.protocolLabel].twitter}
+                    target="_blank"
+                    on:mouseenter={() => (clickOnX = true)}
+                    on:mouseleave={() => (clickOnX = false)}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="18"
+                      height="18"
+                      viewBox="0 0 14 14"
+                    >
+                      <rect width="14" height="14" fill="none" />
+                      <g fill="none">
+                        <g clip-path="url(#primeTwitter0)">
+                          <path
+                            fill="currentColor"
+                            d="M11.025.656h2.147L8.482 6.03L14 13.344H9.68L6.294 8.909l-3.87 4.435H.275l5.016-5.75L0 .657h4.43L7.486 4.71zm-.755 11.4h1.19L3.78 1.877H2.504z"
+                          />
+                        </g>
+                        <defs>
+                          <clipPath id="primeTwitter0">
+                            <path fill="#fff" d="M0 0h14v14H0z" />
+                          </clipPath>
+                        </defs>
+                      </g>
+                    </svg>
+                  </a>
+                {/if}
+                {#if protocolInfo[item?.protocolLabel].website}
+                  <a
+                    href={protocolInfo[item?.protocolLabel].website}
+                    target="_blank"
+                    on:mouseenter={() => (clickOnX = true)}
+                    on:mouseleave={() => (clickOnX = false)}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="22"
+                      height="22"
+                      viewBox="0 0 256 256"
+                    >
+                      <rect width="14" height="14" fill="none" />
+                      <path
+                        fill="currentColor"
+                        d="M137.54 186.36a8 8 0 0 1 0 11.31l-9.94 10a56 56 0 0 1-79.22-79.27l24.12-24.12a56 56 0 0 1 76.81-2.28a8 8 0 1 1-10.64 12a40 40 0 0 0-54.85 1.63L59.7 139.72a40 40 0 0 0 56.58 56.58l9.94-9.94a8 8 0 0 1 11.32 0m70.08-138a56.08 56.08 0 0 0-79.22 0l-9.94 9.95a8 8 0 0 0 11.32 11.31l9.94-9.94a40 40 0 0 1 56.58 56.58l-24.12 24.14a40 40 0 0 1-54.85 1.6a8 8 0 1 0-10.64 12a56 56 0 0 0 76.81-2.26l24.12-24.12a56.08 56.08 0 0 0 0-79.24Z"
+                      />
+                    </svg>
+                  </a>
+                {/if}
+              </div>
+            </div>
+            <div class="flex justify-between">
+              <div class="text-sm font-medium">Points</div>
+              <div class="text-right font-medium text_00000099">
+                {item.points}
+              </div>
+            </div>
+            <div class="flex justify-between">
+              <div class="text-sm font-medium">Estimated value</div>
+              <div class="text-right font-medium text_00000099">
+                <TooltipNumber number={item.price * item.points} type="value" />
+              </div>
+            </div>
+          </div>
+        {/each}{/each}
     {/if}
   </div>
 </ErrorBoundary>
