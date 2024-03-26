d680392d7841ce3a7816ba8030ad5bf20b956c1d
binhnguyen2501 <binhq6@gmail.com>
2024-03-26 13:17:59 +0700

    update logic theme

--- src/UI/Footer/DarkModeFooter.svelte
+++ src/UI/Footer/DarkModeFooter.svelte
@@ -20,6 +20,10 @@
     darkMode
       ? window.document.body.classList.add("dark")
       : window.document.body.classList.remove("dark");
+
+    auto = false;
+    isAutoDarkMode.update((n) => (n = false));
+    localStorage.setItem("auto_theme", "false");
   };
 
   const handleToggleAuto = () => {
@@ -54,7 +58,6 @@
   <input
     checked={darkMode}
     on:click={handleSwitchDarkMode}
-    disabled={auto}
     type="checkbox"
     id="theme-toggle"
     class="hidden"
