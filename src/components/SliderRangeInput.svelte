3ff0c54c4aea3a8f4d41a56bfed037fc2ac8bbe7
binhnguyen2501 <binhq6@gmail.com>
2024-03-27 13:22:22 +0700

    update slide range

--- src/components/SliderRangeInput.svelte
+++ src/components/SliderRangeInput.svelte
@@ -1,34 +1,28 @@
 <script>
-  export let currentInput;
+  export let currentPrice;
+  export let lowerPrice;
+  export let upperPrice;
 
-  const handleCalculateCurrentInput = (input) => {
-    // console.log(input);
-    const max = input.upperPrice;
-    const min = input.lowerPrice;
-
-    return;
-  };
-
-  $: calculatedCurrentInput = handleCalculateCurrentInput(currentInput);
+  $: console.log({
+    currentPrice,
+    upperPrice,
+    lowerPrice,
+  });
 </script>
 
-<div class="w-[90%] h-1">
+<div class="w-[90%] h-1 relative bg-gray-200 rounded-lg appearance-none">
   <div
-    class="relative w-full h-1 bg-gray-200 rounded-lg appearance-none cursor-pointer"
+    class={`absolute top-[50%] -translate-y-1/2 h-1 left-[] w-[] bg-blue-400`}
   >
     <div
-      class="absolute top-[50%] left-[20%] -translate-y-1/2 h-1 w-[60%] bg-blue-400"
-    >
-      <div
-        class={`absolute top-[50%] left-0 -translate-y-1/2 -translate-x-1/2 z-1 w-2 h-2 rounded-full bg-blue-400`}
-      />
-      <div
-        class={`absolute top-[50%] left-[] -translate-y-1/2 -translate-x-1/2 z-1 w-1 h-3 bg-yellow-300`}
-      />
-      <div
-        class={`absolute top-[50%] right-0 -translate-y-1/2 -translate-x-1/2 z-1 w-2 h-2 rounded-full bg-blue-400`}
-      />
-    </div>
+      class={`absolute top-[50%] left-[] -translate-y-1/2 -translate-x-1/2 z-1 w-2 h-2 rounded-full bg-blue-400`}
+    />
+    <div
+      class={`absolute top-[50%] left-[] -translate-y-1/2 -translate-x-1/2 z-1 w-1 h-3 bg-yellow-300`}
+    />
+    <div
+      class={`absolute top-[50%] right-[] -translate-y-1/2 -translate-x-1/2 z-1 w-2 h-2 rounded-full bg-blue-400`}
+    />
   </div>
 </div>
 
