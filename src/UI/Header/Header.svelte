e105cc2ac5b1ab7cc366ee4a626316fcd8336600
binhnguyen2501 <binhq6@gmail.com>
2024-04-03 11:07:51 +0700

    fix aria label

--- src/UI/Header/Header.svelte
+++ src/UI/Header/Header.svelte
@@ -695,6 +695,7 @@
         <span
           class="xl:flex hidden rounded-md w-[24px] h-[24px] p-2 justify-center items-center bg-[#a6b0c3] text-white text-sm"
           role="generic"
+          aria-label="icon"
           use:tooltip={{
             content: `<tooltip-detail text="Use to trigger search" />`,
             allowHTML: true,
