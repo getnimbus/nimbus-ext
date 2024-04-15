2122fef69971e82036d1b758e44d3f4aeebd8d38
binhnguyen2501 <binhq6@gmail.com>
2024-04-15 17:34:56 +0700

    update ui btn

--- src/UI/MainWalletLinks/Ton.svelte
+++ src/UI/MainWalletLinks/Ton.svelte
@@ -146,7 +146,7 @@
 </script>
 
 <div
-  class={`flex items-center justify-center gap-2 text-white border cursor-pointer py-3 px-6 rounded-[12px] min-w-[250px] ${
+  class={`flex items-center justify-center gap-2 text-white border cursor-pointer py-3 px-6 rounded-[12px] w-[250px] ${
     $isDarkMode ? "border-white text-white" : "border-[#27326f] text-[#27326f]"
   }`}
   on:click={handleTonAuth}
