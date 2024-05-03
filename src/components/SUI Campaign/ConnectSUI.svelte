d6165f56456d1ce42bcfae461975fb186e611bd4
binhnguyen2501 <binhq6@gmail.com>
2024-05-03 15:02:34 +0700

    update connect sui

--- src/components/SUI Campaign/ConnectSUI.svelte
+++ src/components/SUI Campaign/ConnectSUI.svelte
@@ -1,8 +1,125 @@
 <script lang="ts">
   import { wait } from "~/entries/background/utils";
   import { triggerFirework } from "~/utils";
+  import { suiWalletInstance } from "~/store";
+  import { SuiConnector, WalletState } from "nimbus-sui-kit";
+  import { useQueryClient } from "@tanstack/svelte-query";
+  import { Toast } from "flowbite-svelte";
+  import { blur } from "svelte/transition";
+  import { nimbus } from "~/lib/network";
 
   import goldImg from "~/assets/Gold4.svg";
+
+  import ReactAdapter from "~/components/ReactAdapter.svelte";
+
+  const queryClient = useQueryClient();
+  const chains = [
+    {
+      id: "sui:mainnet",
+      name: "Mainnet",
+      rpcUrl: "https://fullnode.mainnet.sui.io",
+    },
+  ];
+
+  const onConnectSuccess = (msg) => {
+    console.log("Success connect: ", msg);
+    if ($suiWalletInstance) {
+      ($suiWalletInstance as WalletState).toggleSelect();
+    }
+  };
+
+  const onConnectError = (msg) => {
+    console.error("Error connect", msg);
+    if ($suiWalletInstance) {
+      ($suiWalletInstance as WalletState).toggleSelect();
+    }
+  };
+
+  const widgetConfig = {
+    walletFn: (wallet) => {
+      suiWalletInstance.update((n) => (n = wallet));
+    },
+    onConnectSuccess,
+    onConnectError,
+  };
+
+  const handleSUIAuth = async () => {
+    try {
+      if ($suiWalletInstance) {
+        ($suiWalletInstance as WalletState).toggleSelect();
+      }
+    } catch (e) {
+      console.log("error: ", e);
+    }
+  };
+
+  $: {
+    if (
+      ($suiWalletInstance as WalletState) &&
+      ($suiWalletInstance as WalletState).connected
+    ) {
+      handleGetNonce(($suiWalletInstance as WalletState)?.account?.address);
+    }
+  }
+
+  const handleGetNonce = async (address: string) => {
+    try {
+      const signature = await handleSignAddressMessage(
+        `${Math.floor(Math.random() * 10000)}`
+      );
+      if (signature) {
+        const payload = {
+          signature: signature.signature,
+          bytes: signature.bytes,
+          publicAddress: address?.toLowerCase(),
+        };
+        handleUpdatePublicAddress(payload, address);
+      }
+    } catch (e) {
+      console.error("error: ", e);
+    }
+  };
+
+  const handleSignAddressMessage = async (nonce: string) => {
+    const msg = await ($suiWalletInstance as WalletState).signPersonalMessage({
+      message: new TextEncoder().encode(
+        `I am signing my one-time nonce: ${nonce}`
+      ),
+    });
+    return msg;
+  };
+
+  const handleUpdatePublicAddress = async (payload, address) => {
+    try {
+      let params: any = {
+        kind: "wallet",
+        type: null,
+        userPublicAddress: address,
+        id: data?.uid,
+        info: data?.info,
+        displayName: data?.name,
+      };
+      const res = await nimbus.post("/accounts/link", params);
+      if (res && res?.error) {
+        toastMsg = res?.error;
+        isSuccessToast = false;
+        trigger();
+        return;
+      }
+
+      // TODO: bonus 1k GM points
+      queryClient?.invalidateQueries(["users-me"]);
+      queryClient?.invalidateQueries(["list-address"]);
+      queryClient.invalidateQueries(["list-bundle"]);
+      queryClient.invalidateQueries(["link-socials"]);
+
+      toastMsg = "Your are successfully connect your Sui wallet!";
+      isSuccessToast = false;
+      trigger();
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
   let openScreenBonusScore = false;
   let bonusScore = 0;
@@ -13,10 +130,29 @@
     await wait(2000);
     openScreenBonusScore = false;
   };
+
+  let toastMsg = "";
+  let isSuccessToast = false;
+  let counter = 3;
+  let showToast = false;
+
+  const trigger = () => {
+    showToast = true;
+    counter = 3;
+    timeout();
+  };
+
+  const timeout = () => {
+    if (--counter > 0) return setTimeout(timeout, 1000);
+    showToast = false;
+    toastMsg = "";
+    isSuccessToast = false;
+  };
 </script>
 
 <div
   class="flex items-center gap-2 text-white bg-[#4BA2FF] py-1 px-2 rounded-[10px] cursor-pointer"
+  on:click={handleSUIAuth}
 >
   Connect SUI
   <div
@@ -26,6 +162,14 @@
     <img src={goldImg} alt="" class="w-6 h-6" />
   </div>
 </div>
+
+<ReactAdapter
+  element={SuiConnector}
+  config={widgetConfig}
+  autoConnect={false}
+  {chains}
+  integrator="svelte-example"
+/>
 
 {#if openScreenBonusScore}
   <div
@@ -46,6 +190,51 @@
         You have received {bonusScore} Bonus GM Points
       </div>
     </div>
+  </div>
+{/if}
+
+{#if showToast}
+  <div class="fixed top-3 right-3 w-full" style="z-index: 2147483648;">
+    <Toast
+      transition={blur}
+      params={{ amount: 10 }}
+      position="top-right"
+      color={isSuccessToast ? "green" : "red"}
+      bind:open={showToast}
+    >
+      <svelte:fragment slot="icon">
+        {#if isSuccessToast}
+          <svg
+            aria-hidden="true"
+            class="w-5 h-5"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+            ><path
+              fill-rule="evenodd"
+              d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+              clip-rule="evenodd"
+            /></svg
+          >
+          <span class="sr-only">Check icon</span>
+        {:else}
+          <svg
+            aria-hidden="true"
+            class="w-5 h-5"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+            ><path
+              fill-rule="evenodd"
+              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+              clip-rule="evenodd"
+            /></svg
+          >
+          <span class="sr-only">Error icon</span>
+        {/if}
+      </svelte:fragment>
+      {toastMsg}
+    </Toast>
   </div>
 {/if}
 
