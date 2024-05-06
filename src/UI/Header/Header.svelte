4850d86edd3d4356c176e1185a95a0476bf9e6e2
binhnguyen2501 <binhq6@gmail.com>
2024-05-06 13:02:09 +0700

    remove sigout

--- src/UI/Header/Header.svelte
+++ src/UI/Header/Header.svelte
@@ -437,13 +437,10 @@
       localStorage.removeItem("sui_token");
       localStorage.removeItem("ton_token");
       localStorage.removeItem("evm_token");
-      handleSignOut();
     },
     onSuccess(data) {
       if (data) {
         handleSetUserData(data);
-      } else {
-        handleSignOut();
       }
     },
   });
