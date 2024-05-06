e522c231d4ba0eeb9b8f60090196df4c811c802f
binhnguyen2501 <binhq6@gmail.com>
2024-05-06 12:47:46 +0700

    update query

--- src/UI/Header/Header.svelte
+++ src/UI/Header/Header.svelte
@@ -448,6 +448,16 @@
     },
   });
 
+  $: {
+    if (
+      !$queryUserInfo.isError &&
+      $queryUserInfo &&
+      $queryUserInfo?.data !== undefined
+    ) {
+      handleSetUserData($queryUserInfo?.data);
+    }
+  }
+
   const handleSetUserData = (data) => {
     localStorage.setItem("public_address", data?.publicAddress);
     userPublicAddress.update((n) => (n = data?.publicAddress));
