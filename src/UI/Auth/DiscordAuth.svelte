8d9d51ce71f7b3635424b5ded40e315c5fbb1eaf
binhnguyen2501 <binhq6@gmail.com>
2024-05-09 22:30:18 +0700

    update discord redirect

--- src/UI/Auth/DiscordAuth.svelte
+++ src/UI/Auth/DiscordAuth.svelte
@@ -3,7 +3,7 @@
 
   const handleRedirectDiscordAuth = () => {
     window.location.assign(
-      "https://discord.com/oauth2/authorize?client_id=1236967408204517396&response_type=code&redirect_uri=https%3A%2F%2Fbeta.nimbus-ext.pages.dev&scope=identify+guilds+guilds.members.read"
+      "https://discord.com/oauth2/authorize?client_id=1236967408204517396&response_type=code&redirect_uri=https%3A%2F%2Fapp.getnimbus.io&scope=identify+guilds+guilds.members.read"
     );
   };
 </script>
