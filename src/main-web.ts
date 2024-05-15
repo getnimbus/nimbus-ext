2c458abf6501dc7a74656de19b7e8a21d48cb0e2
binhnguyen2501 <binhq6@gmail.com>
2024-05-15 16:39:56 +0700

    update sentry

--- src/main-web.ts
+++ src/main-web.ts
@@ -13,8 +13,8 @@ mixpanel.init("d56364b743cd70634fe5bea51e1d7e1c", {
 WebApp.ready();
 
 Sentry.init({
-  dsn: "https://o4505729367801856.ingest.us.sentry.io/4506455493246976",
-  integrations: [new Sentry.BrowserTracing()],
+  dsn: "https://314310ebdb5bff4a14a386f1eb6d47b2@o4505729367801856.ingest.us.sentry.io/4506455493246976",
+  integrations: [Sentry.browserTracingIntegration()],
   // debug: true,
   release: "v1.0.0", // TODO: Update version here
   tracePropagationTargets: ["localhost"],
